0838431ae2126cb2dec7c6ea09f0917c7fdc9160
YM_Thon <32301831+Sunda001@users.noreply.github.com>
2018-11-02 08:34:44 +0700

    Create coub.py
    gif with sound

---
+++ plugins/coub.py
@@ -0,0 +1,18 @@
+import config
+import requests
+import random
+
+bot = config.bot
+bot_username = config.bot_username
+def coub(msg):
+    if msg.get('text'):
+        if msg['text'].startswith('/coub ') or msg['text'].startswith('!coub '):
+          text = msg['text'][6:]
+          try:
+            r = requests.get(f'https://coub.com/api/v2/search/coubs?q={text}')
+            content = random.choice(r.json()['coubs'])
+            links = content['permalink']
+            title = content['title']
+            bot.sendMessage(msg['chat']['id'], f'*{title}*[\u00AD](https://coub.com/v/{links})', reply_to_message_id=msg['message_id'])
+          except IndexError:
+            bot.sendMessage(msg['chat']['id'], 'Found', reply_to_message_id=msg['message_id'])
