d18db1d501886cdd7f6615a10374fb40cff7d71e
Alisson Lauffer <alissonvitortc@gmail.com>
2018-11-06 14:53:53 -0200

    Some updates to db handler

--- db_handler.py
+++ db_handler.py
@@ -1,4 +1,5 @@
 import sqlite3
+from sqlite3 import OperationalError
 
 conn = sqlite3.connect('bot.db', check_same_thread=False)
 
@@ -6,13 +7,15 @@ cursor = conn.cursor()
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS chats (
                                                     chat_id,
-                                                    type,
                                                     welcome,
-                                                    welcome_set,
+                                                    welcome_enabled,
                                                     goodbye,
-                                                    goodbye_set,
+                                                    goodbye_enabled,
                                                     ia
                                                     )''')
+
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS users (user_id, ia)''')
 
 
 cursor.execute('CREATE TABLE IF NOT EXISTS was_restarted_on (chat_id, message_id)')
@@ -27,15 +30,29 @@ def chat_exists(chat_id):
         return False
 
 
+def user_exists(user_id):
+    try:
+        cursor.execute('SELECT * FROM chats WHERE user_id = (?)', (user_id,))
+        cursor.fetchall()[0]
+        return True
+    except (OperationalError, IndexError):
+        return False
+
+
 def del_restarted():
     cursor.execute('DELETE FROM was_restarted_on')
     conn.commit()
 
 
 def add_chat(chat_type, chat_id):
-    if not chat_exists(chat_id):
-        cursor.execute('INSERT INTO chats (chat_id, type) VALUES (?,?)', (chat_id, chat_type))
-        conn.commit()
+    if chat_type == 'supergroup' or chat_type == 'group':
+        if not chat_exists(chat_id):
+            cursor.execute('INSERT INTO chats (chat_id) VALUES (?)', (chat_id,))
+            conn.commit()
+    else:
+        if not user_exists(chat_id):
+            cursor.execute('INSERT INTO users (user_id) VALUES (?)', (chat_id,))
+            conn.commit()
 
 
 def get_restarted():
