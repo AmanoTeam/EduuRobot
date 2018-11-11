66ee15db3c953451a6a9bccc376d2ed8aa52b9d3
Alisson Lauffer <alissonvitortc@gmail.com>
2018-11-11 21:23:11 -0200

    Updates

--- plugins/inlines.py
+++ plugins/inlines.py
@@ -126,7 +126,8 @@ def inlines(msg):
             except TelegramError:
                 articles = [InlineQueryResultArticle(
                     id='a', title='Texto com erros de formatação.', input_message_content=InputTextMessageContent(message_text='Ocorreu um erro. provavelmente porque você usou uma tag não suportada, ou porque você esqueceu de fechar alguma tag. As tags suportadas são estas: <b>, <i>, <code>, <a> e <pre>.', parse_mode='html'))]
-
+                bot.answerInlineQuery(msg['id'], results=articles)
+                
 
         elif msg['query'].startswith('/yt '):
             articles = []
