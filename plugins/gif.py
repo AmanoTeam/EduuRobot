969280da2c914d4cd8bb15e026646cdb7b9ad2cd
Alisson Lauffer <alissonvitortc@gmail.com>
2018-11-11 14:00:20 -0200

    Handle empty results for /gif

--- plugins/gif.py
+++ plugins/gif.py
@@ -13,9 +13,11 @@ def gif(msg):
             text = msg['text'][5:]
             rjson = requests.get(
                 "http://api.giphy.com/v1/gifs/search", params=dict(q=text, api_key=giphy_key, limit=7)).json()
-            res = random.choice(rjson["data"])
-            result = res["images"]["original_mp4"]["mp4"]
-            bot.sendVideo(
-                chat_id=msg['chat']['id'],
-                video=result,
-                reply_to_message_id=msg['message_id'])
+            if rjson["data"]:
+                res = random.choice(rjson["data"])
+                result = res["images"]["original_mp4"]["mp4"]
+                bot.sendVideo(msg['chat']['id'], result,
+                              reply_to_message_id=msg['message_id'])
+            else:
+                bot.sendMessage(msg['chat']['id'], "Sem resultados",
+                                reply_to_message_id=msg['message_id'])
