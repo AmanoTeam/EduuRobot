cbb233bbc00c8e7fb5d2213986d9111248e04076
Alisson Lauffer <alissonvitortc@gmail.com>
2018-11-12 01:36:33 -0200

    Test

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -66,7 +66,7 @@ def youtube(msg):
                                           reply_to_message_id=msg['message_id'])['message_id']
                 try:
                     if 'youtu.be' not in text and 'youtube.com' not in text:
-                        yt = ydl.extract_info('ytsearch:'+text, download=False)
+                        yt = ydl.extract_info('ytsearch:'+text, download=False)['entries']
                     else:
                         url = text
                         yt = ydl.extract_info(url, download=False)
