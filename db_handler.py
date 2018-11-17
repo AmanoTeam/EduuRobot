c0b9a9ba0f116bdafc7525584d0d95753f395f99
Alisson Lauffer <alissonvitortc@gmail.com>
2018-11-16 22:56:03 -0200

    Move get_welcome and set_welcome to welcome.py

--- db_handler.py
+++ db_handler.py
@@ -64,19 +64,3 @@ def get_restarted():
 def set_restarted(chat_id, message_id):
     cursor.execute('INSERT INTO was_restarted_on VALUES (?, ?)', (chat_id, message_id))
     conn.commit()
-
-
-def get_welcome(chat_id):
-    cursor.execute('SELECT (welcome, welcome_enabled) FROM chats WHERE chat_id = (?)', (chat_id,))
-    try:
-        return cursor.fetchall()[0]
-    except IndexError:
-        return None
-
-
-def set_welcome(chat_id, welcome):
-    try:
-        cursor.execute('UPDATE chats SET welcome = ? welcome_set = True WHERE chat_id = ?', (welcome, chat_id))
-    finally:
-        cursor.execute('INSERT INTO chats (chat_id, welcome) VALUES (?, ?)', (chat_id, welcome))
-    conn.commit()
--- plugins/welcome.py
+++ plugins/welcome.py
@@ -1,6 +1,24 @@
 import config
+from db_handler import conn, cursor
 
 bot = config.bot
+
+
+def get_welcome(chat_id):
+    cursor.execute('SELECT (welcome, welcome_enabled) FROM chats WHERE chat_id = (?)', (chat_id,))
+    try:
+        return cursor.fetchall()[0]
+    except IndexError:
+        return None
+
+
+def set_welcome(chat_id, welcome):
+    try:
+        cursor.execute('UPDATE chats SET welcome = ? welcome_set = True WHERE chat_id = ?', (welcome, chat_id))
+    finally:
+        cursor.execute('INSERT INTO chats (chat_id, welcome) VALUES (?, ?)', (chat_id, welcome))
+    conn.commit()
+
 
 def welcome(msg):
     pass
