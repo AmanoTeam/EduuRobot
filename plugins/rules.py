d73dfb726f9b525d374e547e4c2aec5df4bb0610
Alisson Lauffer <alissonvitortc@gmail.com>
2018-12-09 21:27:45 -0200

    Initial rules plugin

---
+++ plugins/rules.py
@@ -0,0 +1,52 @@
+import config
+from db_handler import conn, cursor
+from .admins import isAdmin
+
+bot = config.bot
+bot_username = config.bot_username
+bot_id = config.bot_id
+
+
+def get_rules(chat_id):
+    cursor.execute('SELECT rules FROM chats WHERE chat_id = (?)', (chat_id,))
+    try:
+        return cursor.fetchall()[0]
+    except IndexError:
+        return None
+
+
+def set_rules(chat_id, rules):
+    cursor.execute('UPDATE chats SET rules = ? WHERE chat_id = ?', (rules, chat_id))
+    conn.commit()
+
+
+def rules(msg):
+    if msg.get('text'):
+
+        if msg['text'].startswith('/start rules_'):
+            chat_id = msg['text'].split('_')[1]
+            rules = get_rules(chat_id) or 'Sem regras!'
+
+            bot.sendMessage(msg['chat']['id'], rules, 'Markdown')
+
+
+        elif msg['text'] == '/rules' or msg['text'] == '!rules' or msg['text'] == '/regras' or msg['text'] == '!regras' or msg['text'] == '/regras@'+bot_username or msg['text'] == '/rules@'+bot_username:
+            rules = get_rules(msg['chat']['id']) or 'Sem regras!'
+
+            bot.sendMessage(msg['chat']['id'], rules, 'Markdown'
+                            reply_to_message_id=msg['message_id'])
+
+
+        elif msg['text'] == '/defrules' or msg['text'] == '!defrules' or msg['text'] == '/defregras' or msg['text'] == '!defregras' or msg['text'] == '/defregras@'+bot_username or msg['text'] == '/defrules@'+bot_username:
+            if isAdmin(msg['chat']['id'], msg['from']['id'])[0]:
+                if len(msg['text'].split()) == 1:
+                    bot.sendMessage(msg['chat']['id'], 'Uso: /defregras Regras do grupo (suporta Markdown)',
+                                    reply_to_message_id=msg['message_id'])
+                elif msg['text'].split()[1] == 'reset':
+                    set_rules(msg['chat']['id'], None)
+                    bot.sendMessage(msg['chat']['id'], 'As regras do grupo foram redefinidas com sucesso.',
+                                    reply_to_message_id=msg['message_id'])
+                else:
+                    set_rules(msg['chat']['id'], msg['text'].split(' ', 1)[1])
+                    bot.sendMessage(msg['chat']['id'], 'As regras do grupo foram definidas com sucesso.',
+                                    reply_to_message_id=msg['message_id'])
