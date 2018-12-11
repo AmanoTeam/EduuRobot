2388062fce2785956cd1dc0d6458a29dfe9c6bdb
Alisson Lauffer <alissonvitortc@gmail.com>
2018-12-10 22:22:52 -0200

    Fix sed plugin

--- plugins/sed.py
+++ plugins/sed.py
@@ -13,7 +13,7 @@ def sed(msg):
             replace_with = exp[2]
             flags = exp[3] if len(exp) > 3 else 'g'
 
-            res = re.sub(pattern, replace_with, msg['reply_to_message']['text'], flags)
+            res = re.sub(pattern, replace_with, msg['reply_to_message']['text'], flags=flags)
 
             bot.sendMessage(msg['chat']['id'], f'<pre>{html.escape(res)}</pre>', 'html',
                             reply_to_message_id=msg['reply_to_message']['message_id'])
