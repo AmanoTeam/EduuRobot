f16dc12d2ae30b14a7aec0fd894d53c549999c67
Alisson Lauffer <alissonvitortc@gmail.com>
2018-12-14 15:44:02 -0200

    Initial git plugin

---
+++ plugins/git.py
@@ -0,0 +1,21 @@
+import requests
+from config import bot
+
+def git(msg):
+    if msg.get('text'):
+        if msg['text'].startswith('/git ') or text.startswith('!git '):
+            text = msg['text'][5:]
+            res = requests.get('https://api.github.com/users/' + text).json()
+            if not res.get('login'):
+                return bot.sendMessage(chat_id, 'Usuário "{}" não encontrado.'.format(text),
+                                       reply_to_message_id=msg['message_id'])
+            else:
+                bot.sendMessage(chat_id, '''*Nome:* `{}`
+*Login:* `{}`
+*Localização:* `{}`
+*Tipo:* `{}`
+*Bio:* `{}`'''.format(result['name'], result['login'],
+                       result['location'], result['type'],
+                       result['bio']), 'Markdown',
+                                reply_to_message_id=msg['message_id'])
+            return True
