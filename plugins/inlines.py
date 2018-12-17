31c7c400d9b3c70486600808eb00a47d171fcc6e
Eduardo Fonseca <contact@eduuu.ga>
2018-12-17 13:08:33 -0200

    Organizing inline texts

--- plugins/inlines.py
+++ plugins/inlines.py
@@ -270,7 +270,7 @@ def inlines(msg):
                             first_name) + '</code>\n<b>ID:</b> <code>' + str(
                             user_id) + '</code>\n<b>Username:</b> <code>' + username + '</code>', parse_mode="HTML")),
                 InlineQueryResultArticle(
-                    id='b', title='duck', description='searching with ddg engine',
+                    id='b', title='duck', description='Pesquise no DuckDuckGo via inline.',
                     input_message_content=dict(message_text='<b>Uso:</b> <code>@{} duck</code> - Pesquise no DuckDuckGo via inline.'.format(bot_username), parse_mode='HTML')),
                 InlineQueryResultArticle(
                     id='c', title='faces', description='Mostra uma lista de carinhas ¯\\_(ツ)_/¯',
@@ -282,19 +282,19 @@ def inlines(msg):
                     input_message_content=dict(
                         message_text='<b>Uso:</b> <code>@{} hidemsg</code> - Envie uma mensagem que se for apagada em até 1 minuto não aparece nas <i>ações recentes</i> do grupo.'.format(bot_username), parse_mode='HTML')),
                 InlineQueryResultArticle(
-                    id='e', title='html', description='Formata um texto usando HTML',
+                    id='e', title='html', description='Formata um código em HTML.',
                     input_message_content=dict(message_text='<b>Uso:</b> <code>@{} html</code> - Formata um código em HTML.'.format(bot_username), parse_mode='HTML')),
                 InlineQueryResultArticle(
-                    id='f', title='img', description='searching image using google image', input_message_content=dict(
+                    id='f', title='img', description='Buscador de imagens via inline.', input_message_content=dict(
                         message_text='<b>Uso:</b> <code>@{} img</code> - Buscador de imagens via inline.'.format(bot_username), parse_mode='HTML')),
                 InlineQueryResultArticle(
-                    id='g', title='ip', description='Exibe informações de um IP informado',
+                    id='g', title='ip', description='Exibe informações de determinado IP/URL.',
                     input_message_content=dict(message_text='<b>Uso:</b> <code>@{} ip</code> - Exibe informações de determinado IP/URL.'.format(bot_username), parse_mode='HTML')),
                 InlineQueryResultArticle(
-                    id='h', title='proxy', description='searching proxy', input_message_content=dict(
+                    id='h', title='proxy', description='Exibe uma lista de proxys de vários países.', input_message_content=dict(
                         message_text='<b>Uso:</b> <code>@{} proxy</code> - Exibe uma lista de proxys de vários países.'.format(bot_username), parse_mode='HTML')),
                 InlineQueryResultArticle(
-                    id='i', title='yt', description='Pesquisar vídeos no YouTube', input_message_content=dict(
+                    id='i', title='yt', description='Pesquise vídeos no YouTube via inline.', input_message_content=dict(
                         message_text='<b>Uso:</b> <code>@{} yt</code> - Pesquise vídeos no YouTube via inline.'.format(bot_username), parse_mode='HTML'))
             ]
 
