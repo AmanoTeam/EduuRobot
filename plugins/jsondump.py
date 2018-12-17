7ee57571fbb6500357bb81fb71972ef8bd6258c1
Alisson Lauffer <alissonvitortc@gmail.com>
2018-12-17 00:01:58 -0200

    Use html for jsondump, remove try and except and duplicated code

--- plugins/jsondump.py
+++ plugins/jsondump.py
@@ -1,6 +1,7 @@
 import json
 import config
 import os
+import html
 
 bot = config.bot
 bot_username = config.bot_username
@@ -10,22 +11,14 @@ def jsondump(msg):
     if msg.get('text'):
         if msg['text'].startswith('/jsondump') or msg['text'].startswith('!jsondump') or msg[
             'text'] == '/jsondump@' + bot_username:
-            try:
-                if '-f' not in msg['text']:
-                    bot.sendMessage(msg['chat']['id'], '`' + json.dumps(msg, indent=2, sort_keys=False) + '`',
-                                    'Markdown', reply_to_message_id=msg['message_id'])
-                else:
-                    bot.sendChatAction(msg['chat']['id'], 'upload_document')
-                    with open('dump.json', 'wb') as i:
-                        i.write(bytes(str(json.dumps(msg, indent=2, sort_keys=False)), 'utf-8'))
-                    bot.sendDocument(msg['chat']['id'], open('dump.json', 'rb'), reply_to_message_id=msg['message_id']
-                                     )
-                    os.remove('dump.json')
-            except:
+            msgjson = json.dumps(msg, indent=2, sort_keys=False)
+            if '-f' not in msg['text'] and len(msgjson) < 4080:
+                    bot.sendMessage(msg['chat']['id'], '<pre>' + html.escape(msgjson) + '</pre>',
+                                    'html', reply_to_message_id=msg['message_id'])
+            else:
                 bot.sendChatAction(msg['chat']['id'], 'upload_document')
                 with open('dump.json', 'wb') as i:
-                    i.write(bytes(str(json.dumps(msg, indent=2, sort_keys=False)), 'utf-8'))
-                bot.sendDocument(msg['chat']['id'], open('dump.json', 'rb'),
-                                 reply_to_message_id=msg['message_id'])
+                    i.write(msgjson.encode())
+                bot.sendDocument(msg['chat']['id'], open('dump.json', 'rb'), reply_to_message_id=msg['message_id'])
                 os.remove('dump.json')
             return True
