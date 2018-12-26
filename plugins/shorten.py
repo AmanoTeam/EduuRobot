45ee652eb99a80b575f29f01f43dd4fb2db05daf
Alisson Lauffer <alissonvitortc@gmail.com>
2018-12-26 17:46:54 -0200

    Change 🇧🇷.ml url

--- plugins/shorten.py
+++ plugins/shorten.py
@@ -10,9 +10,9 @@ def shorten(msg):
             text = msg['text'][9:]
             if text == '':
                 bot.sendMessage(msg['chat']['id'],
-                                '*Uso:* `/shorten google.com` - _Encurta uma URL. Powered by_ 🇧🇷.ml', 'Markdown',
+                                '*Uso:* `/shorten google.com` - _Encurta uma URL. Powered by _🇧🇷.ml', 'Markdown',
                                 reply_to_message_id=msg['message_id'])
             else:
-                r = requests.post('https://amanosite.000webhostapp.com/api/', data=dict(url=text))
+                r = requests.post('https://xn--f77h6a.ml/api/', data=dict(url=text))
                 bot.sendMessage(msg['chat']['id'], '*Resultado:* {}'.format(r.json()['result']), 'Markdown',
                                 reply_to_message_id=msg['message_id'])
