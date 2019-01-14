912d9088f6f126a4cb0acd5569050d9bca6750f9
Alisson Lauffer <alissonvitortc@gmail.com>
2019-01-14 20:45:58 -0200

    Try to fix ReDoS

--- plugins/sed.py
+++ plugins/sed.py
@@ -1,11 +1,13 @@
 from config import bot
 import re
 import html
-from multiprocessing import Process
+from multiprocessing import Process, Manager
+import ctypes
 
 
-def replace():
-    globals()['res'] = re.sub(pattern, replace_with, text, count=count, flags=rflags)
+def replace(res, pattern, replace_with, text, count, rflags):
+    res.value = re.sub(pattern, replace_with, text, count=count, flags=rflags)
+
 
 
 def sed(msg):
@@ -33,14 +35,17 @@ def sed(msg):
             if msg['reply_to_message'].get('caption'):
                 text = msg['reply_to_message']['caption']
 
-            p = Process(target=replace)
+            manager = Manager()
+            res = manager.Value(ctypes.c_char_p, None)
+
+            p = Process(target=replace, args=(res, pattern, replace_with, text, count, rflags))
             p.start()
             p.join(0.2)
             p.terminate()
 
-            if globals().get('res') is None:
-                bot.sendMessage(msg['chat']['id'], 'Ocorreu um problema com o seu padrão regex.',
-                                reply_to_message_id=msg['reply_to_message']['message_id'])
+            if res.value is None:
+                bot.sendMessage(msg['chat']['id'], 'Ocorreu um erro com o seu padrão regex.',
+                                reply_to_message_id=msg['message_id'])
             else:
                 bot.sendMessage(msg['chat']['id'], f'<pre>{html.escape(res)}</pre>', 'html',
                                 reply_to_message_id=msg['reply_to_message']['message_id'])
