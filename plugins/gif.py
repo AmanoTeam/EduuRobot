ecd7a69e11ffa69a9a6ac8264d26ee39a46c1870
Alisson Lauffer <alissonvitortc@gmail.com>
2019-04-26 23:05:06 -0300

    requests > aiohttp (gif.py)

--- plugins/gif.py
+++ plugins/gif.py
@@ -19,7 +19,7 @@
 
 import random
 
-import requests
+import aiohttp
 
 from config import bot, keys
 
@@ -30,8 +30,9 @@ async def gif(msg):
     if msg.get('text'):
         if msg['text'].startswith('/gif ') or msg['text'].startswith('!gif '):
             text = msg['text'][5:]
-            rjson = requests.get(
-                "http://api.giphy.com/v1/gifs/search", params=dict(q=text, api_key=giphy_key, limit=7)).json()
+            async with aiohttp.ClientSession() as session:
+                r = await session.get("http://api.giphy.com/v1/gifs/search", params=dict(q=text, api_key=giphy_key, limit=7))
+                rjson = await r.json()
             if rjson["data"]:
                 res = random.choice(rjson["data"])
                 result = res["images"]["original_mp4"]["mp4"]
