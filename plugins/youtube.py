8eaef6ebf5bb10fa06ad48d03d95faa13c0a9838
Yusuf_M_Thon_iD <32301831+Sunda001@users.noreply.github.com>
2019-06-09 22:35:42 +0700

    yt search
    fixing list results

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -56,17 +56,20 @@ async def youtube(msg):
 
         if msg['text'].startswith('/yt '):
             res = await search_yt(msg['text'][4:])
-            vids = ''
             if len(res) == 0:
-                vids += "Nenhum resultado foi encontrado"
+                vids = "Nenhum resultado foi encontrado"
+                await bot.sendMessage(msg['chat']['id'], vids, 'HTML',
+                                      reply_to_message_id=msg['message_id'],
+                                      disable_web_page_preview=True)
             else:
+                vids = ''
                 for num, i in enumerate(res):
                     vids += '{}: <a href="{}">{}</a>\n'.format(num + 1, i['url'], i['title'])
             
-            await bot.sendMessage(msg['chat']['id'], vids, 'HTML',
-                                  reply_to_message_id=msg['message_id'],
-                                  disable_web_page_preview=True)
-            return True
+                await bot.sendMessage(msg['chat']['id'], vids, 'HTML',
+                                      reply_to_message_id=msg['message_id'],
+                                      disable_web_page_preview=True)
+                return True
 
 
         elif msg['text'].split()[0] == '/ytdl':
