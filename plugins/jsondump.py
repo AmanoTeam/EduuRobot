d4d2e342d1c535f2eb7cf4fa659288152b05fde5
Alisson Lauffer <alissonvitortc@gmail.com>
2019-07-12 00:32:10 -0300

    [jsondump.py] Send as document if result is too big

--- plugins/jsondump.py
+++ plugins/jsondump.py
@@ -1,8 +1,16 @@
+import os
 import html
 from config import prefix
 from pyrogram import Client, Filters
 
 
 @Client.on_message(Filters.command("jsondump", prefix))
-async def start_private(client, message):
-    await message.reply(f"<code>{html.escape(str(message))}</code>", parse_mode="HTML")
+async def jsondump(client, message):
+    if len(str(message)) < 3000 and "-f" not in message.command:
+        await message.reply(f"<code>{html.escape(str(message))}</code>", parse_mode="HTML")
+    else:
+        fname = f"dump-{message.chat.id}.json"
+        with open(fname, "w") as f:
+            f.write(str(message))
+        await message.reply_document(fname)
+        os.remove(fname)
