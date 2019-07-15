19d5e297aef680e8cc080e9f6fdce741c2928126
Alisson Lauffer <alissonvitortc@gmail.com>
2019-07-15 01:57:26 -0300

    Add speedtest plugin, add !cmd sudo command

--- plugins/sudos.py
+++ plugins/sudos.py
@@ -2,6 +2,8 @@ import io
 import os
 import re
 import sys
+import html
+import asyncio
 import traceback
 from pyrogram import Client, Filters
 from contextlib import redirect_stdout
@@ -15,6 +17,21 @@ async def sudos(client, message):
     await message.reply("Test")
 
 
+@Client.on_message(Filters.command("cmd", prefix) & Filters.user(sudoers))
+async def run_cmd(client, message):
+    cmd = re.split(r"[\n ]+", message.text, 1)[1]
+    if re.match('(?i)poweroff|halt|shutdown|reboot', cmd):
+        res = 'Comando proibido.'
+    else:
+        proc = await asyncio.create_subprocess_shell(cmd,
+                                                     stdout=asyncio.subprocess.PIPE,
+                                                     stderr=asyncio.subprocess.PIPE)
+        stdout, stderr = await proc.communicate()
+        res = ("<b>Output:</b>\n<code>{}</code>".format(stdout.decode())  if stdout else '') + (
+               "\n\n<b>Errors:</b>\n<code>{}</code>".format(stderr.decode())  if stderr else '')
+    await message.reply(res)
+
+
 @Client.on_message(Filters.command("eval", prefix) & Filters.user(sudoers))
 async def evals(client, message):
     code = re.split(r"[\n ]+", message.text, 1)[1]
@@ -23,7 +40,7 @@ async def evals(client, message):
         res = await eval(code[:isasync.start(1)] + code[isasync.end(1):]) if isasync else eval(code)
     except Exception as e:
         res = str(e)
-    await message.reply(res if res != None else "Executed")
+    await message.reply(str(res))
 
 
 @Client.on_message(Filters.command("exec", prefix) & Filters.user(sudoers))
@@ -35,7 +52,7 @@ async def execs(client, message):
         try:
             await locals()["__ex"](client, message)
         except:
-            traceback.print_exc()
+            return await message.reply(html.escape(traceback.format_exc()), parse_mode="HTML")
     await message.reply(strio.getvalue() or "ok")
 
 
