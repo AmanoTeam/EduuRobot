4458c699291c1e2151423124fdda4604e0832ba3
Alisson Lauffer <alissonvitortc@gmail.com>
2019-11-21 15:00:27 -0300

    Implement back button callback

--- plugins/start.py
+++ plugins/start.py
@@ -24,20 +24,45 @@ async def start(client, message):
                                  reply_markup=keyboard)
 
 
+@Client.on_callback_query(Filters.callback_data("start_back"))
+async def start_back(client, message):
+    _ = GetLang(message).strs
+    # TODO: Create a function to generate translatable keyboards instead of duplicating code fragments
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(_("start.commands_btn"), callback_data="commands")] +
+        [InlineKeyboardButton(_("start.infos_btn"), callback_data="infos")],
+        [InlineKeyboardButton(_("start.language_btn"), callback_data="chlang")] +
+        [InlineKeyboardButton(_("start.add_chat_btn"), url="https://t.me/{}?startgroup=new")],
+    ])
+    await message.message.edit(_("start.private"),
+                               reply_markup=keyboard)
+
+
 @Client.on_callback_query(Filters.callback_data("commands"))
 async def commands(client, message):
-    await message.message.edit("commands")
+    _ = GetLang(message).strs
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(_("general.back_btn"), callback_data="start_back")]
+    ])
+    await message.message.edit("commands", reply_markup=keyboard)
 
 
 @Client.on_callback_query(Filters.callback_data("infos"))
 async def infos(client, message):
     _ = GetLang(message).strs
     res = _("start.info_page").format(
-                version=version
-            )
-    await message.message.edit(res, disable_web_page_preview=True)
+        version=version
+    )
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(_("general.back_btn"), callback_data="start_back")]
+    ])
+    await message.message.edit(res, reply_markup=keyboard, disable_web_page_preview=True)
 
 
 @Client.on_callback_query(Filters.callback_data("chlang"))
 async def chlang(client, message):
-    await message.message.edit("chlang")
+    _ = GetLang(message).strs
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(_("general.back_btn"), callback_data="start_back")]
+    ])
+    await message.message.edit("chlang", reply_markup=keyboard)
