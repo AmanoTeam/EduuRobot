9b382eedd27f03ab37205dacb20212d071bf82e5
Alisson Lauffer <alissonvitortc@gmail.com>
2019-11-21 02:24:37 -0300

    Localize dice plugin

--- plugins/dice.py
+++ plugins/dice.py
@@ -3,11 +3,13 @@ import random
 from pyrogram import Client, Filters
 
 from config import prefix
+from localization import GetLang
 
 
 @Client.on_message(Filters.command(["dice", "dados"], prefix))
 async def dice(client, message):
+    _ = GetLang(message).strs
     dicen = random.randint(1, 6)
-    await message.reply_text("🎲 The dice stopped at the number: {number}".format(
+    await message.reply_text(_("dice.result").format(
         number=dicen
     ))
