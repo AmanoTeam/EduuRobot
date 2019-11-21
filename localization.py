8fe1d7c94d7932df77d1e535b5e082b6b0b61190
Alisson Lauffer <alissonvitortc@gmail.com>
2019-11-20 22:21:30 -0300

    Return english strings if lang key equals to ""

--- localization.py
+++ localization.py
@@ -44,4 +44,4 @@ class GetLang:
         self.dic = langdict.get(self.lang, langdict["en-US"])
 
     def strs(self, string):
-        return self.dic.get(string, string)
+        return self.dic.get(string) or langdict["en-US"].get(string) or string
