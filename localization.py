f43c1845498771b7ec7f547da9053dede80edc8c
Alisson Lauffer <alissonvitortc@gmail.com>
2019-12-17 03:04:50 -0300

    Some db additions regarding language changing

--- localization.py
+++ localization.py
@@ -1,11 +1,28 @@
 import json
 from glob import glob
+from dbh import dbc
+
 
 enabled_locales = [
     "en-US",  # English (United States)
     "pt-BR",  # Portuguese (Brazil)
     "pt-BR2",  # Portuguese (Brazil, informal version)
 ]
+
+
+def get_lang(chat_id, chat_type):
+    if chat_type == "private":
+        dbc.execute("SELECT chat_lang FROM users where user_id = ?", (chat_id,))
+        ul = dbc.fetchone()
+    elif chat_type == "group" or chat_type == "supergroup": # groups and supergroups share the same table
+        dbc.execute("SELECT chat_lang FROM groups where chat_id = ?", (chat_id,))
+        ul = dbc.fetchone()
+    elif chat_type == "channel":
+        dbc.execute("SELECT chat_lang FROM channels where channel_id = ?", (chat_id,))
+        ul = dbc.fetchone()
+    else:
+        raise TypeError("Unknown chat type '%s'." % chat_type)
+    return ul[0] if ul else None
 
 
 def cache_localizations(files):
@@ -27,15 +44,18 @@ langdict = cache_localizations(jsons)
 
 class GetLang:
     def __init__(self, msg):
-        # try to get user lang from language_code, if it does not exist, use en-US
-        self.lang = msg.from_user.language_code or "en-US"
+        lang = get_lang(msg.chat.id, msg.chat.type)
+        if msg.chat.type == "private":
+            self.lang = lang or msg.from_user.language_code or "en-US"
+        else:
+            self.lang = lang or "en-US"
         # User has a language_code without hyphen
         if len(self.lang.split("-")) == 1:
             # Try to find a language that starts with the provided language_code
             for locale_ in enabled_locales:
                 if locale_.startswith(self.lang):
                     self.lang = locale_
-        if self.lang.split("-")[1].islower():
+        elif self.lang.split("-")[1].islower():
             self.lang = self.lang.split("-")
             self.lang[1] = self.lang[1].upper()
             self.lang = "-".join(self.lang)
