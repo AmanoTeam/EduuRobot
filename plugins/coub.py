cb7d650fb5968d3a0c89bf1fa0fdda7e4ec95434
Alisson Lauffer <alissonvitortc@gmail.com>
2020-01-20 18:24:06 -0300

    Port gif and coub plugins

---
+++ plugins/coub.py
@@ -0,0 +1,25 @@
+import random
+import aiohttp
+
+from pyrogram import Client, Filters
+
+from config import prefix
+from localization import GetLang
+
+
+@Client.on_message(Filters.command("coub", prefix))
+async def coub(client, message):
+    _ = GetLang(message).strs
+    text = message.text[6:]
+    async with aiohttp.ClientSession() as session:
+        r = await session.get('https://coub.com/api/v2/search/coubs',
+                              params=dict(q=text))
+        rjson = await r.json()
+    try:
+        content = random.choice(rjson['coubs'])
+        links = content['permalink']
+        title = content['title']
+    except IndexError:
+        await message.reply_text(_("general.no_results"))
+    else:
+        await message.reply_text(f'**{title}**[\u00AD](https://coub.com/v/{links})')
---
+++ plugins/gif.py
@@ -0,0 +1,30 @@
+import random
+import aiohttp
+import logging
+import sys
+
+from pyrogram import Client, Filters
+
+from localization import GetLang
+from config import GIPHY_API_KEY, prefix
+
+
+if GIPHY_API_KEY:
+
+    @Client.on_message(Filters.command("gif", prefix))
+    async def gif(client, message):
+        _ = GetLang(message).strs
+        text = message.text[5:]
+        async with aiohttp.ClientSession() as session:
+            r = await session.get("http://api.giphy.com/v1/gifs/search",
+                                  params=dict(q=text, api_key=GIPHY_API_KEY, limit=7))
+            rjson = await r.json()
+        if rjson["data"]:
+            res = random.choice(rjson["data"])
+            result = res["images"]["downsized_medium"]["url"]
+            await message.reply_animation(result)
+        else:
+            await message.reply_text(_("general.no_results"))
+
+else:
+    logging.warning(f"[{__name__}] You need to fill GIPHY_API_KEY in your config file in order to use the plugin.")
