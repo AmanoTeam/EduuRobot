4dcf9d723b6dcb1b54d384f8868b007c9bd48338
Alisson Lauffer <alissonvitortc@gmail.com>
2020-07-21 18:04:26 -0300

    Replace Giphy with Tenor

--- plugins/gif.py
+++ plugins/gif.py
@@ -1,15 +1,14 @@
-import random
 import aiohttp
 import logging
 
 from pyrogram import Client, Filters, Message
 
 from localization import GetLang
-from config import GIPHY_API_KEY, prefix
+from config import TENOR_API_KEY, prefix
 
 
-if not GIPHY_API_KEY:
-    logging.warning(f"[{__name__}] You need to fill GIPHY_API_KEY in your config file in order to use this plugin.")
+if not TENOR_API_KEY:
+    logging.warning(f"[{__name__}] You need to fill TENOR_API_KEY in your config file in order to use this plugin.")
 
 
 @Client.on_message(Filters.command("gif", prefix))
@@ -17,13 +16,12 @@ async def gif(c: Client, m: Message):
     _ = GetLang(m).strs
     text = m.text[5:]
     async with aiohttp.ClientSession() as session:
-        r = await session.get("http://api.giphy.com/v1/gifs/search",
-                              params=dict(q=text, api_key=GIPHY_API_KEY, limit=7))
+        r = await session.get("https://api.tenor.com/v1/random",
+                              params=dict(q=text, key=TENOR_API_KEY, limit=1))
         rjson = await r.json()
-    if rjson["data"]:
-        res = random.choice(rjson["data"])
-        result = res["images"]["downsized_medium"]["url"]
-        await m.reply_animation(result)
+    if rjson["results"]:
+        res = rjson["results"][0]["media"][0]["mediumgif"]["url"]
+        await m.reply_animation(res)
     else:
         await m.reply_text(_("general.no_results"))
 
