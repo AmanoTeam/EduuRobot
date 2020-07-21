ee2fa93c7ee487c792c36a85c77e5bed9da58beb
Alisson Lauffer <alissonvitortc@gmail.com>
2020-07-21 17:45:11 -0300

    Unwrap gif plugin and just show a warning in console.

--- plugins/gif.py
+++ plugins/gif.py
@@ -8,22 +8,22 @@ from localization import GetLang
 from config import GIPHY_API_KEY, prefix
 
 
-if GIPHY_API_KEY:
+if not GIPHY_API_KEY:
+    logging.warning(f"[{__name__}] You need to fill GIPHY_API_KEY in your config file in order to use this plugin.")
 
-    @Client.on_message(Filters.command("gif", prefix))
-    async def gif(c: Client, m: Message):
-        _ = GetLang(m).strs
-        text = m.text[5:]
-        async with aiohttp.ClientSession() as session:
-            r = await session.get("http://api.giphy.com/v1/gifs/search",
-                                  params=dict(q=text, api_key=GIPHY_API_KEY, limit=7))
-            rjson = await r.json()
-        if rjson["data"]:
-            res = random.choice(rjson["data"])
-            result = res["images"]["downsized_medium"]["url"]
-            await m.reply_animation(result)
-        else:
-            await m.reply_text(_("general.no_results"))
 
-else:
-    logging.warning(f"[{__name__}] You need to fill GIPHY_API_KEY in your config file in order to use the plugin.")
+@Client.on_message(Filters.command("gif", prefix))
+async def gif(c: Client, m: Message):
+    _ = GetLang(m).strs
+    text = m.text[5:]
+    async with aiohttp.ClientSession() as session:
+        r = await session.get("http://api.giphy.com/v1/gifs/search",
+                              params=dict(q=text, api_key=GIPHY_API_KEY, limit=7))
+        rjson = await r.json()
+    if rjson["data"]:
+        res = random.choice(rjson["data"])
+        result = res["images"]["downsized_medium"]["url"]
+        await m.reply_animation(result)
+    else:
+        await m.reply_text(_("general.no_results"))
+
