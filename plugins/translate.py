6813c6ca5da99703fe0a053578df4bab23b7e2b6
Alisson Lauffer <alissonvitortc@gmail.com>
2020-09-28 19:09:00 -0300

    Replace Yandex translator with googletrans

--- plugins/translate.py
+++ plugins/translate.py
@@ -21,60 +21,57 @@ import html
 
 import aiohttp
 
-from config import bot, keys
+from googletrans import Translator, LANGUAGES
 
-traducao = keys['yandex']
-
-idiomas = [
-    'az', 'ml', 'sq', 'mt', 'am', 'mk', 'en', 'mi', 'ar', 'mr', 'hy', 'mhr', 'af', 'mn', 'eu', 'de', 'ba', 'ne', 'be',
-    'no', 'bn', 'pa', 'my', 'pap', 'bg', 'fa', 'bs', 'pl', 'cy', 'pt', 'hu', 'ro', 'vi', 'ru', 'ht', 'ceb', 'gl', 'sr',
-    'nl', 'si', 'mrj', 'sk', 'el', 'sl', 'ka', 'sw', 'gu', 'su', 'da', 'tg', 'he', 'th', 'yi', 'tl', 'id', 'ta', 'ga',
-    'tt', 'it', 'te', 'is', 'tr', 'es', 'udm', 'kk', 'uz', 'kn', 'uk', 'ca', 'ur', 'ky', 'fi', 'zh', 'fr', 'ko', 'hi',
-    'xh', 'hr', 'km', 'cs', 'lo', 'sv', 'la', 'gd', 'lv', 'et', 'lt', 'eo', 'lb', 'jv', 'mg', 'ja', 'ms'
-]
-
+from config import bot
 
 def get_lang(text):
     if len(text.split()) > 0:
         lang = text.split()[0]
-        if lang.split('-')[0] not in idiomas:
-            lang = 'pt'
-        if len(lang.split('-')) > 1:
-            if lang.split('-')[1] not in idiomas:
-                lang = 'pt'
+        if lang.split("-")[0] not in LANGUAGES:
+            lang = "pt"
+        if len(lang.split("-")) > 1:
+            if lang.split("-")[1] not in LANGUAGES:
+                lang = "pt"
     else:
-        lang = 'pt'
+        lang = "pt"
     return lang
 
 
 async def translate(msg):
-    if msg.get('text'):
-        if msg['text'].startswith('/tr ') or msg['text'] == '/tr':
-            text = msg['text'][4:]
+    if msg.get("text"):
+        if msg["text"].startswith("/tr ") or msg["text"] == "/tr":
+            translator = Translator()
+            text = msg["text"][4:]
             lang = get_lang(text)
-            if msg.get('reply_to_message'):
-                if msg['reply_to_message'].get('text'):
-                    text = msg['reply_to_message']['text']
-                if msg['reply_to_message'].get('caption'):
-                    text = msg['reply_to_message']['caption']
+            if msg.get("reply_to_message"):
+                if msg["reply_to_message"].get("text"):
+                    text = msg["reply_to_message"]["text"]
+                if msg["reply_to_message"].get("caption"):
+                    text = msg["reply_to_message"]["caption"]
             else:
-                text = text.replace(lang, '', 1).strip() if text.startswith(lang) else text
+                text = text.replace(lang, "", 1).strip() if text.startswith(lang) else text
 
             if len(text) > 0:
-                sent = await bot.sendMessage(msg['chat']['id'], 'Traduzindo...',
-                                             reply_to_message_id=msg['message_id'])
-                async with aiohttp.ClientSession() as session:
-                    r = await session.post("https://translate.yandex.net/api/v1.5/tr.json/translate",
-                                           data=dict(key=traducao, lang=lang, text=text))
-                    req = await r.json()
+                sent = await bot.sendMessage(msg["chat"]["id"], "Traduzindo...",
+                                             reply_to_message_id=msg["message_id"])
+                langs = {}
 
-                await bot.editMessageText((msg['chat']['id'], sent['message_id']),
-                                          '''<b>Idioma:</b> {}
-<b>Tradução:</b> <code>{}</code>'''.format(req['lang'], html.escape(req['text'][0])),
-                                          parse_mode='HTML')
+                if len(lang.split("-")) > 1:
+                    langs["src"] = lang.split("-")[0]
+                    langs["dest"] = lang.split("-")[1]
+                else:
+                    langs["dest"] = lang
+
+                trres = translator.translate(text, **langs)
+                text = trres.text
+                await bot.editMessageText((msg["chat"]["id"], sent["message_id"]),
+                                          """<b>Idioma:</b> {} -> {}
+<b>Tradução:</b> <code>{}</code>""".format(trres.src, trres.dest, html.escape(trres.text)),
+                                          parse_mode="HTML")
 
             else:
-                await bot.sendMessage(msg['chat']['id'],
-                                      'Uso: /tr <idioma> texto para traduzir (pode ser usado em resposta a uma mensagem).',
-                                      reply_to_message_id=msg['message_id'])
+                await bot.sendMessage(msg["chat"]["id"],
+                                      "Uso: /tr <idioma> texto para traduzir (pode ser usado em resposta a uma mensagem).",
+                                      reply_to_message_id=msg["message_id"])
             return True
