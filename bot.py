e4c17b69b4dd1544a51ff48a4cd3411b691126f9
Alisson Lauffer <alissonvitortc@gmail.com>
2020-10-11 08:17:53 -0300

    Don't redefine variable from outer scope

--- bot.py
+++ bot.py
@@ -7,15 +7,15 @@ from config import TOKEN, disabled_plugins, log_chat, API_ID, API_HASH
 with open("version.txt") as f:
     version = f.read().strip()
 
+client = Client("bot", API_ID, API_HASH, bot_token=TOKEN, plugins=dict(root="plugins", exclude=disabled_plugins))
 
-async def run_client(client):
+
+async def run_client():
     await client.start()
     await client.send_message(log_chat, "**Bot started**\n\n"
                                         f"**Version:** {version}")
     await idle()
 
-client = Client("bot", API_ID, API_HASH, bot_token=TOKEN, plugins=dict(root="plugins", exclude=disabled_plugins))
-
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run_client(client))
+    loop.run_until_complete(run_client())
