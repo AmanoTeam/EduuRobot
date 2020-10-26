16841f11fc3f481f2bf1430f15318356e6a8be8a
Alisson Lauffer <alissonvitortc@gmail.com>
2020-10-25 23:10:53 -0300

    [bot.py] Simplify main code
    - As of Pyrogram 1.0.x, we can mix async and sync calls, so we will not need to explicitly init the Client with an async function.
    - If __name__ != "__main__", the bot will immediately stop after a successful start (This will help sanitizing code syntax, as all modules will be loaded).

--- bot.py
+++ bot.py
@@ -1,5 +1,3 @@
-import asyncio
-
 from pyrogram import Client, idle
 
 from config import TOKEN, disabled_plugins, log_chat, API_ID, API_HASH
@@ -7,15 +5,8 @@ from config import TOKEN, disabled_plugins, log_chat, API_ID, API_HASH
 with open("version.txt") as f:
     version = f.read().strip()
 
-client = Client("bot", API_ID, API_HASH, bot_token=TOKEN, plugins=dict(root="plugins", exclude=disabled_plugins))
-
-
-async def run_client():
-    await client.start()
-    await client.send_message(log_chat, "**Bot started**\n\n"
-                                        f"**Version:** {version}")
-    await idle()
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run_client())
+with Client("bot", API_ID, API_HASH, bot_token=TOKEN, plugins=dict(root="plugins", exclude=disabled_plugins)) as client:
+    if __name__ == "__main__":
+        client.send_message(log_chat, "**Bot started**\n\n"
+                                      f"**Version:** {version}")
+        idle()
