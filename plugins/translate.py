3f0639e6199d4aad5490b7c494b8291b798c24c8
Alisson Lauffer <alissonvitortc@gmail.com>
2020-12-09 03:59:03 -0300

    Remove one indentation level from translate.py

--- plugins/translate.py
+++ plugins/translate.py
@@ -33,28 +33,27 @@ async def translate(c: Client, m: Message, strings):
     else:
         text = text.replace(lang, "", 1).strip() if text.startswith(lang) else text
 
-    if text:
-        sent = await m.reply_text(strings("translating"),
+    if not text:
+        return await m.reply_text(strings("translate_usage"),
+                                  reply_to_message_id=m.message_id,
+                                  parse_mode="HTML")
+
+    sent = await m.reply_text(strings("translating"),
                                   reply_to_message_id=m.message_id)
-        langs = {}
+    langs = {}
 
-        if len(lang.split("-")) > 1:
-            langs["src"] = lang.split("-")[0]
-            langs["dest"] = lang.split("-")[1]
-        else:
-            langs["dest"] = lang
-
-        trres = translator.translate(text, **langs)
-        text = trres.text
-
-        res = html.escape(text)
-        await sent.edit_text(strings("translation").format(
-                from_lang=trres.src,
-                to_lang=trres.dest,
-                translation=res),
-                             parse_mode="HTML")
-
+    if len(lang.split("-")) > 1:
+        langs["src"] = lang.split("-")[0]
+        langs["dest"] = lang.split("-")[1]
     else:
-        await m.reply_text(strings("translate_usage"),
-                           reply_to_message_id=m.message_id,
-                           parse_mode="HTML")
+        langs["dest"] = lang
+
+    trres = translator.translate(text, **langs)
+    text = trres.text
+
+    res = html.escape(text)
+    await sent.edit_text(strings("translation").format(
+            from_lang=trres.src,
+            to_lang=trres.dest,
+            translation=res),
+                         parse_mode="HTML")
