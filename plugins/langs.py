159c004633e4ca75b830f4631a60b1105c057b69
Alisson Lauffer <alissonvitortc@gmail.com>
2020-12-09 03:37:49 -0300

    Move language related strings to langs.json

--- plugins/langs.py
+++ plugins/langs.py
@@ -3,7 +3,7 @@ from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMa
 
 from functools import partial
 
-from localization import langdict, set_db_lang, use_chat_lang, get_locale_string
+from localization import langdict, set_db_lang, use_chat_lang, get_locale_string, default_language
 
 
 def gen_langs_kb():
@@ -30,7 +30,7 @@ async def chlang(c: Client, m: CallbackQuery, strings):
         *gen_langs_kb(),
         [InlineKeyboardButton(strings("back_btn", context="general"), callback_data="start_back")]
     ])
-    await m.message.edit_text(strings("language_changer_private", context="general"), reply_markup=keyboard)
+    await m.message.edit_text(strings("language_changer_private"), reply_markup=keyboard)
 
 
 @Client.on_callback_query(filters.regex("^set_lang "))
@@ -39,9 +39,11 @@ async def set_user_lang(c: Client, m: CallbackQuery, strings):
     lang = m.data.split()[1]
     set_db_lang(m.message.chat.id, m.message.chat.type, lang)
 
-    strings = partial(get_locale_string, langdict[lang]["general"], lang, "general")
+    strings = partial(get_locale_string,
+                      langdict[lang].get("langs", langdict[default_language]["langs"]),
+                      lang, "langs")
 
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(strings("back_btn"), callback_data="start_back")]
+        [InlineKeyboardButton(strings("back_btn", context="general"), callback_data="start_back")]
     ])
     await m.message.edit_text(strings("language_changed_successfully"), reply_markup=keyboard)
