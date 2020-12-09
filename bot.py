32c8cead3c80d0433c7864f6212a5350c0f708e5
Alisson Lauffer <alissonvitortc@gmail.com>
2020-12-09 01:27:55 -0300

    Reorganize bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,12 @@ from config import TOKEN, disabled_plugins, log_chat, API_ID, API_HASH
 with open("version.txt") as f:
     version = f.read().strip()
 
-with Client("bot", API_ID, API_HASH, bot_token=TOKEN, plugins=dict(root="plugins", exclude=disabled_plugins)) as client:
+
+client = Client("bot", API_ID, API_HASH,
+                bot_token=TOKEN,
+                plugins=dict(root="plugins", exclude=disabled_plugins))
+
+with client:
     if __name__ == "__main__":
         client.me = client.get_me()
         client.send_message(log_chat, "**Bot started**\n\n"
