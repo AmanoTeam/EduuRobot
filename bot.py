35228e6c4c78ff0b5b7db9aefb59095fd26760df
Alisson Lauffer <alissonvitortc@gmail.com>
2020-12-09 01:35:39 -0300

    Don't exit if unable to send messages to log_chat.

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
+import logging
+
 from pyrogram import Client, idle
+
+from pyrogram.errors.exceptions.bad_request_400 import BadRequest
 
 from config import TOKEN, disabled_plugins, log_chat, API_ID, API_HASH
 
@@ -13,6 +17,9 @@ client = Client("bot", API_ID, API_HASH,
 with client:
     if __name__ == "__main__":
         client.me = client.get_me()
-        client.send_message(log_chat, "**Bot started**\n\n"
-                                      f"**Version:** {version}")
+        try:
+            client.send_message(log_chat, "**Bot started**\n\n"
+                                          f"**Version:** {version}")
+        except BadRequest:
+            logging.warning("Unable to send message to log_chat.")
         idle()
