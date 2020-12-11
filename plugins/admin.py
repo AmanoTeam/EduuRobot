e5585341fc304b08918e2c7a7136a127fe69956b
Alisson Lauffer <alissonvitortc@gmail.com>
2020-12-11 18:59:27 -0300

    Remove trailing whitespaces and fix some formatting issues

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,65 +1,64 @@
 from pyrogram import Client, filters
+from pyrogram.types import ChatPermissions, Message
+
 from config import prefix
-from pyrogram.types import ChatPermissions
 from utils import require_admin
-from pyrogram.types import Message as message
+
 
 @Client.on_message(filters.command("pin", prefix))
 @require_admin(permissions=["can_pin_messages"])
-async def pin(client, message):
-    await client.pin_chat_message(
-    message.chat.id,
-    message.reply_to_message.message_id
+async def pin(c: Client, m: Message):
+    await c.pin_chat_message(
+        m.chat.id,
+        m.reply_to_message.message_id
     )
-    
-    
+
+
 @Client.on_message(filters.command("unpin", prefix))
 @require_admin(permissions=["can_pin_messages"])
-async def unpin(client, message):
-    await client.unpin_chat_message(
-    message.chat.id,
-    message.reply_to_message.message_id
+async def unpin(c: Client, m: Message):
+    await c.unpin_chat_message(
+        m.chat.id,
+        m.reply_to_message.message_id
     )
-    
-    
+
+
 @Client.on_message(filters.command("unpinall", prefix))
 @require_admin(permissions=["can_pin_messages"])
-async def unpinall(client, message):
-    await client.unpin_all_chat_messages(
-        message.chat.id
+async def unpinall(c: Client, m: Message):
+    await c.unpin_all_chat_messages(
+        m.chat.id
     )
-    
-       
+
+
 @Client.on_message(filters.command("ban", prefix))
 @require_admin(permissions=["can_restrict_members"])
-async def ban(client, message):
-    await client.kick_chat_member(message.chat.id, message.reply_to_message.from_user.id)
+async def ban(c: Client, m: Message):
+    await c.kick_chat_member(m.chat.id, m.reply_to_message.from_user.id)
 
-    
+
 @Client.on_message(filters.command("kick", prefix))
 @require_admin(permissions=["can_restrict_members"])
-async def kick(client, message):
-    await client.kick_chat_member(message.chat.id, message.reply_to_message.from_user.id)
-    await message.chat.unban_member(message.reply_to_message.from_user.id)
-    
+async def kick(c: Client, m: Message):
+    await c.kick_chat_member(m.chat.id, m.reply_to_message.from_user.id)
+    await m.chat.unban_member(m.reply_to_message.from_user.id)
+
+
 @Client.on_message(filters.command("unban", prefix))
 @require_admin(permissions=["can_restrict_members"])
-async def unban(client, message):
-    await message.chat.unban_member(message.reply_to_message.from_user.id
-    )
-    
+async def unban(c: Client, m: Message):
+    await m.chat.unban_member(m.reply_to_message.from_user.id)
+
+
 @Client.on_message(filters.command("mute", prefix))
 @require_admin(permissions=["can_restrict_members"])
-async def mute(client, message):
-    await client.restrict_chat_member(message.chat.id,
-     message.reply_to_message.from_user.id,
-     ChatPermissions(can_send_messages=False),
-       )
-  
-    
+async def mute(c: Client, m: Message):
+    await c.restrict_chat_member(m.chat.id,
+                                 m.reply_to_message.from_user.id,
+                                 ChatPermissions(can_send_messages=False))
+
+
 @Client.on_message(filters.command("unmute", prefix))
 @require_admin(permissions=["can_restrict_members"])
-async def unmute(client, message):
-    await message.chat.unban_member(message.reply_to_message.from_user.id
-    )     
-           
+async def unmute(c: Client, m: Message):
+    await m.chat.unban_member(m.reply_to_message.from_user.id)
