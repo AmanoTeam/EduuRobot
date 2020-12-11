aedafb6c23fdd8e66bad28612b13e35ef5895177
iiiiii1wepfj <itaykotik@gmail.com>
2020-12-11 22:30:33 +0200

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -4,6 +4,7 @@ from pyrogram.types import ChatPermissions
 
 
 @Client.on_message(filters.command("pin", prefix))
+@require_admin()
 async def pin(client, message):
     await client.pin_chat_message(
     message.chat.id,
@@ -12,6 +13,7 @@ async def pin(client, message):
     
     
 @Client.on_message(filters.command("unpin", prefix))
+@require_admin()
 async def unpin(client, message):
     await client.unpin_chat_message(
     message.chat.id,
@@ -20,6 +22,7 @@ async def unpin(client, message):
     
     
 @Client.on_message(filters.command("unpinall", prefix))
+@require_admin()
 async def unpinall(client, message):
     await client.unpin_all_chat_messages(
         message.chat.id
@@ -27,6 +30,7 @@ async def unpinall(client, message):
     
        
 @Client.on_message(filters.command("ban", prefix))
+@require_admin()
 async def ban(client, message):
     await message.chat.kick_member(
     user_id=user_id
@@ -34,17 +38,20 @@ async def ban(client, message):
 
     
 @Client.on_message(filters.command("kick", prefix))
+@require_admin()
 async def kick(client, message):
     await message.client.kick_chat_member(chat_id, user_id, int(time.time() + 60))
     )
     
 @Client.on_message(filters.command("unban", prefix))
+@require_admin()
 async def unban(client, message):
     await message.chat.unban_member(
     user_id=user_id
     )
     
 @Client.on_message(filters.command("mute", prefix))
+@require_admin()
 async def mute(client, message):
     await client.restrict_chat_member(message.chat.id,
     message.from_user.id,
@@ -53,6 +60,7 @@ async def mute(client, message):
   
     
 @Client.on_message(filters.command("unmute", prefix))
+@require_admin()
 async def unmute(client, message):
     await message.chat.unban_member(
     user_id=user_id
