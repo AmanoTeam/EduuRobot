ea39faaa34a955b831a98c01e2d3f1642bd6960e
Anonymous <69723581+New-dev0@users.noreply.github.com>
2020-12-16 00:05:02 +0530

    Add telegraph module (#110)
    Co-authored-by: iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
    Co-authored-by: Alisson Lauffer <alissonvitortc@gmail.com>

---
+++ plugins/telegraph.py
@@ -0,0 +1,20 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from config import prefix
+import telegraph
+from telegraph import Telegraph, upload_file, exceptions
+
+telegraph = Telegraph()
+r = telegraph.create_account(short_name="@EduuRobot on telegram")
+
+@Client.on_message(filters.command("telegraph", prefix))
+async def telegraph(c: Client, m: Message):
+    if m.reply_to_message:
+        if m.reply_to_message.photo or m.reply_to_message.video or m.reply_to_message.animation:
+            d_file = await m.reply_to_message.download()
+            media_urls = upload_file(d_file)
+            tele_link = "https://telegra.ph" + media_urls[0]
+            await m.reply_text(tele_link)
+    else:
+        await m.reply_text("Please Reply to Photo or Video")
