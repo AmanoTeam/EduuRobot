c6b3b3fb1d0f9dd2c9243b8dd3148fe7a407421b
Anonymous <69723581+New-dev0@users.noreply.github.com>
2020-12-15 20:33:26 +0530

    Update telegraph.py

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -1,1 +1,22 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
+from config import prefix
+import telegraph
+from telegraph import Telegraph, upload_file, exceptions
+
+telegraph = Telegraph()
+r = telegraph.create_account(short_name="EduuRobot")
+
+@Client.on_message(filters.command("telegraph", prefix))
+async def telegraph(c: Client, m: Message):
+    if m.reply_to_message:
+        if m.reply_to_message.photo or m.reply_to_message.video:
+            d_file = m.reply_to_message.download()
+            media_urls = upload_file(d_file)
+            tele_link = "https://telegra.ph".format(media_urls[0])
+            await m.reply_text(tele_link)
+    else:
+        await m.reply_text("Please Reply to Photo or Video")
+            
+    
