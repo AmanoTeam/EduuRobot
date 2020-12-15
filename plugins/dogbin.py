0589234940976c26eeea255afe7f27e61e953d95
Alisson Lauffer <alissonvitortc@gmail.com>
2020-12-15 15:20:14 -0300

    [dogbin.py] Move from requests to httpx and remove duplicate code blocks

--- plugins/dogbin.py
+++ plugins/dogbin.py
@@ -1,9 +1,9 @@
-import requests
-
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
 from config import prefix
+from consts import http
+
 
 @Client.on_message(filters.command("paste", prefix))
 async def dogbin(c: Client, m: Message):
@@ -11,21 +11,14 @@ async def dogbin(c: Client, m: Message):
         if m.reply_to_message.document:
             tfile = m.reply_to_message
             to_file = await tfile.download()
-            m_list = None
-            with open(to_file,'rb') as fd:
-                m_list = fd.readlines()
-            mean=""
-            for s in m_list:
-                mean+= s.decode('UTF-8') +"\r\n"
-            url = f"https://del.dog/documents"
-            r = requests.post(url,data = mean.encode('UTF-8')).json()
-            url = f"https://del.dog/{r['key']}"
-            await m.reply_text(url,disable_web_page_preview=True)
+            with open(to_file, 'rb') as fd:
+                mean = fd.read().decode('UTF-8')
         if m.reply_to_message.text:
             mean = m.reply_to_message.text
-            url = f"https://del.dog/documents"
-            r = requests.post(url,data = mean.encode('UTF-8')).json()
-            url = f"https://del.dog/{r['key']}"
-            await m.reply_text(url,disable_web_page_preview=True)
+
+        url = f"https://del.dog/documents"
+        r = await http.post(url, data=mean.encode('UTF-8'))
+        url = f"https://del.dog/{r.json()['key']}"
+        await m.reply_text(url, disable_web_page_preview=True)
     else:
         await m.reply_text("Please Reply to text or document.")
