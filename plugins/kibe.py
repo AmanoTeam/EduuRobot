c6f66377ff16fed8700199c4628ec06853a21da7
Rian Felipe Celestrino <39718272+RianFC@users.noreply.github.com>
2021-02-16 01:25:16 -0300

    add /kibe tgs suport

--- plugins/kibe.py
+++ plugins/kibe.py
@@ -18,11 +18,14 @@
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import os
+import time
+import math
 
 from amanobot.exception import TelegramError
 from amanobot.namedtuple import InlineKeyboardMarkup
 
 from config import bot, bot_username
+from PIL import Image
 
 
 async def kibe(msg):
@@ -52,32 +55,51 @@ async def kibe(msg):
 
 
         elif msg['text'].startswith('/kibe') or msg['text'].startswith('!kibe'):
-            if msg.get('reply_to_message') and msg['reply_to_message'].get('sticker'):
-                user = msg['from']
-                file_id = msg['reply_to_message']['sticker']['file_id']
-                await bot.download_file(file_id, str(msg['from']['id']) + '_kibe_sticker.png')
-                packname = "a" + str(user['id']) + "_by_" + bot_username
-                if len(msg['text'][5:]) > 0:
-                    sticker_emoji = msg['text'].split()[1]
-                else:
-                    try:
-                        sticker_emoji = msg['reply_to_message']['sticker']['emoji']
-                    except KeyError:
-                        os.remove(str(msg['from']['id']) + "_kibe_sticker.png")
-                        return await bot.sendMessage(msg['chat']['id'],
-                                                     'You need to define a emoticon for this sticker, since it doesn\'t have one.',
-                                                     reply_to_message_id=msg['message_id'])
-                success = False
+            if msg.get('reply_to_message'):
+                emoji = msg['text'][5:]
+                rmsg = msg['reply_to_message']
+                packname = "a" + str(msg['from']['id']) + "_by_" + bot_username
+                rsize = False
+                anim = False
+                if rmsg.get('document'):
+                    await bot.download_file(rmsg['document']['file_id'], str(msg['from']['id']) + '_kibe_sticker.png')
+                    name = str(msg['from']['id']) + '_kibe_sticker.png'
+                    rsize = True
+                elif rmsg.get('photo'):
+                    await bot.download_file(rmsg['photo'][-1]['file_id'], str(msg['from']['id']) + '_kibe_sticker.png')
+                    name = str(msg['from']['id']) + '_kibe_sticker.png'
+                    rsize = True
+                elif rmsg.get('sticker'):
+                    if len(emoji) == 0:
+                        emoji = rmsg['sticker']['emoji']
+                    if rmsg['sticker']['is_animated']:
+                        anim = True
+                        await bot.download_file(rmsg['sticker']['file_id'], str(msg['from']['id']) + '_kibe_sticker.tgs')
+                        name = str(msg['from']['id']) + '_kibe_sticker.tgs'
+                        packname = 'an'+packname[1:]
+                    else:
+                        await bot.download_file(rmsg['sticker']['file_id'], str(msg['from']['id']) + '_kibe_sticker.tgs')
+                        name = str(msg['from']['id']) + '_kibe_sticker.tgs'
+                        rsize = True
+                if not emoji:
+                    emoji = "👍"
+                if rsize:
+                    name = await resize_photo(name, msg['from']['id'])
+                
                 try:
-                    await bot.addStickerToSet(user_id=user['id'], name=packname,
-                                              png_sticker=open(str(msg['from']['id']) + '_kibe_sticker.png', 'rb'),
-                                              emojis=sticker_emoji)
+                    if anim:
+                        await bot.addStickerToSet(user_id=msg['from']['id'], name=packname,
+                                              tgs_sticker=open(name, 'rb'), emojis=emoji)
+                    else:
+                        await bot.addStickerToSet(user_id=msg['from']['id'], name=packname,
+                                              png_sticker=open(name, 'rb'), emojis=emoji)
                     success = True
                 except TelegramError as e:
                     if e.description == "Bad Request: STICKERSET_INVALID":
-                        await bot.sendMessage(msg['chat']['id'], "Use /make_kibe to create a pack first.",
+                        success = await make_pack(msg, bot, name, anim, packname, emoji)
+                        if not success:
+                            return await bot.sendMessage(msg['chat']['id'], 'An error has occurred.' ,
                                               reply_to_message_id=msg['message_id'])
-                        return
                     elif e.description == "Internal Server Error: sticker set not found":
                         success = True
                     else:
@@ -85,45 +107,57 @@ async def kibe(msg):
                                               reply_to_message_id=msg['message_id'])
                         return
                 finally:
-                    os.remove(str(msg['from']['id']) + "_kibe_sticker.png")
-
+                    os.remove(name)
+                    pass
                 if success:
                     await bot.sendMessage(msg['chat']['id'],
                                           "Sticker successfully added to [pack](t.me/addstickers/%s)" % packname,
                                           parse_mode='markdown', reply_to_message_id=msg['message_id'])
-
-            else:
-                await bot.sendMessage(msg['chat']['id'], "Please reply to a sticker for me to kibe it.")
-            return True
-
-
-        elif msg['text'].startswith('/make_kibe') or msg['text'].startswith('!make_kibe'):
-            user = msg['from']
-            name = user['first_name']
-            name = name[:50]
-            packname = "a" + str(user['id']) + "_by_" + bot_username
-            success = False
-            try:
-                success = await bot.createNewStickerSet(user['id'], packname, name + "'s Kibe @AmanoTeam",
-                                                        png_sticker="https://i.imgur.com/wB1iZFI.png",
-                                                        emojis='©')
-            except TelegramError as e:
-                if e.description == "Bad Request: sticker set name is already occupied":
-                    await bot.sendMessage(msg['chat']['id'],
-                                          "Your pack can be found [here](t.me/addstickers/%s)" % packname,
-                                          parse_mode='markdown', reply_to_message_id=msg['message_id'])
-                elif e.description == "Bad Request: PEER_ID_INVALID":
-                    await bot.sendMessage(msg['chat']['id'], "Contact me in PM first.",
-                                          reply_markup=InlineKeyboardMarkup(inline_keyboard=[
-                                              [dict(text='Start', url="t.me/{}".format(bot_username))]]),
-                                          reply_to_message_id=msg['message_id'])
                 else:
-                    await bot.sendMessage(msg['chat']['id'],
-                                          "Failed to create sticker pack. Possibly due to blek mejik.\n\n" + str(e),
-                                          reply_to_message_id=msg['message_id'])
+                    await bot.sendMessage(msg['chat']['id'], "Please reply to a sticker for me to kibe it.")
+                return True
 
-            if success:
-                await bot.sendMessage(msg['chat']['id'],
-                                      "Sticker pack successfully created. Get it [here](t.me/addstickers/%s)" % packname,
-                                      parse_mode='markdown', reply_to_message_id=msg['message_id'])
-            return True
+async def make_pack(msg, bot, sname, anim, packname, emoji):
+    user = msg['from']
+    name = user['first_name']
+    name = name[:50]
+    try:
+        if anim:
+            success = await bot.createNewStickerSet(user['id'], packname, name + "'s Kibe @AmanoTeam",
+                                                tgs_sticker=open(sname, 'rb'),
+                                                emojis=emoji)
+        else:
+            success = await bot.createNewStickerSet(user['id'], packname, name + "'s Kibe @AmanoTeam",
+                                                png_sticker=open(sname, 'rb'),
+                                                emojis=emoji)
+    except TelegramError as e:
+        success = False
+    return success
+
+async def resize_photo(photo, uid):
+    """ Resize the given photo to 512x512 """
+    image = Image.open(photo)
+    maxsize = (512, 512)
+    if (image.width and image.height) < 512:
+        size1 = image.width
+        size2 = image.height
+        if image.width > image.height:
+            scale = 512 / size1
+            size1new = 512
+            size2new = size2 * scale
+        else:
+            scale = 512 / size2
+            size1new = size1 * scale
+            size2new = 512
+        size1new = math.floor(size1new)
+        size2new = math.floor(size2new)
+        sizenew = (size1new, size2new)
+        image = image.resize(sizenew)
+    else:
+        image.thumbnail(maxsize)
+
+    os.remove(photo)
+
+    image.save(f'{uid}_kibe_sticker.png')
+
+    return f'{uid}_kibe_sticker.png'
