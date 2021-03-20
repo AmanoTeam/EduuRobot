bb1f751c1eb6fbf052d239c42bd43771f6a85a60
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-03-20 12:36:40 +0200

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -55,6 +55,12 @@ async def getbotinfo(c: Client, m: Message):
     )
 
 
+    
+@Client.on_message(filters.text & filters.reply & filters.group & filters.command("rt", ""))
+async def rtcommand(c: Client, m: Message):
+    await m.reply(f"🔃 <b>{m.from_user.first_name}</b> retweeted: \n\n 👤 <b>{m.reply_to_message.from_user.first_name}</b>: <i>{m.reply_to_message.text}</i>")
+    
+  
 commands.add_command("mark", "general")
 commands.add_command("html", "general")
 commands.add_command("admins", "general")
