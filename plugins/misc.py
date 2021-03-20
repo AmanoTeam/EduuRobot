9c0d34bdfcc988abad22071b22b54de8a14fbbc1
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-03-20 18:49:03 +0200

    Update plugins/misc.py
    Co-authored-by: Alisson Lauffer <alissonvitortc@gmail.com>

--- plugins/misc.py
+++ plugins/misc.py
@@ -55,7 +55,6 @@ async def getbotinfo(c: Client, m: Message):
     )
 
 
-    
 @Client.on_message(filters.reply & filters.group & filters.regex(r"(?i)^rt$"))
 async def rtcommand(c: Client, m: Message):
     await m.reply(f"🔃 <b>{m.from_user.first_name}</b> retweeted: \n\n 👤 <b>{m.reply_to_message.from_user.first_name}</b>: <i>{m.reply_to_message.text}</i>")
