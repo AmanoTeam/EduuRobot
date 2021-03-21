b582b5c092612b266439a4dea381af643b22c2b3
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-03-21 15:43:00 +0200

    Update warns.py

--- plugins/warns.py
+++ plugins/warns.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from dbh import dbc, db
-from utils import require_admin, time_extract, html_user, commands
+from utils import require_admin
 from config import prefix
 from .admin import get_target_user
 
