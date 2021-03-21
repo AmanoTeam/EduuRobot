0e0791d53fb5b1fdb309f8c1ab316acb6cc73fab
Alisson Lauffer <alissonvitortc@gmail.com>
2021-03-21 19:57:47 -0300

    Fix Pyrogram 1.2.x stacked handlers usage

--- plugins/start.py
+++ plugins/start.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton, Message
+from typing import Union
 
 from config import prefix, version
 from localization import use_chat_lang
@@ -9,36 +10,29 @@ from utils import commands
 @Client.on_message(filters.command("start", prefix))
 @Client.on_callback_query(filters.regex("^start_back$"))
 @use_chat_lang()
-async def start(c: Client, m: Message, strings):
-    if m.chat.type == "private":
+async def start(c: Client, m: Union[Message, CallbackQuery], strings):
+    if isinstance(m, CallbackQuery):
+        msg = m.message
+        method = msg.edit_text
+    else:
+        msg = m
+        method = msg.reply_text
+
+    if msg.chat.type == "private":
         keyboard = InlineKeyboardMarkup(inline_keyboard=[
             [InlineKeyboardButton(strings("commands_btn"), callback_data="commands")] +
             [InlineKeyboardButton(strings("infos_btn"), callback_data="infos")],
             [InlineKeyboardButton(strings("language_btn"), callback_data="chlang")] +
             [InlineKeyboardButton(strings("add_chat_btn"), url=f"https://t.me/{c.me.username}?startgroup=new")],
         ])
-        await m.reply_text(strings("private"),
-                           reply_markup=keyboard)
+        await method(strings("private"),
+                     reply_markup=keyboard)
     else:
         keyboard = InlineKeyboardMarkup(inline_keyboard=[
             [InlineKeyboardButton(strings("start_chat"), url=f"https://t.me/{c.me.username}?start=start")]
         ])
-        await m.reply_text(strings("group"),
-                           reply_markup=keyboard)
-
-
-@Client.on_callback_query(filters.regex("^start_back$"))
-@use_chat_lang()
-async def start_back(c: Client, m: CallbackQuery, strings):
-    # TODO: Create a function to generate translatable keyboards instead of duplicating code fragments
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(strings("commands_btn"), callback_data="commands")] +
-        [InlineKeyboardButton(strings("infos_btn"), callback_data="infos")],
-        [InlineKeyboardButton(strings("language_btn"), callback_data="chlang")] +
-        [InlineKeyboardButton(strings("add_chat_btn"), url=f"https://t.me/{c.me.username}?startgroup=new")],
-    ])
-    await m.message.edit_text(strings("private"),
-                              reply_markup=keyboard)
+        await method(strings("group"),
+                     reply_markup=keyboard)
 
 
 @Client.on_callback_query(filters.regex("^infos$"))
