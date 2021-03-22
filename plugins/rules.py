2a9641a30a4ddb7c1a7457c622b2a4272cc6f1f8
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-03-22 10:56:26 +0200

    Update rules.py

--- plugins/rules.py
+++ plugins/rules.py
@@ -21,17 +21,20 @@ def set_rules(chat_id, rules):
 @Client.on_message(filters.command("setrules", prefix) & filters.group)
 @require_admin(permissions=["can_restrict_members"])
 async def settherules(c: Client, m: Message):
-    set_rules(m.chat.id, m.text.split(None, 1)[1])
-    await m.reply_text(f"the rules for {m.chat.title} was setted successfully")
+    if len(m.text.split()) > 1:
+        set_rules(m.chat.id, m.text.split(None, 1)[1])
+        await m.reply_text(f"the rules for {m.chat.title} was setted successfully")
+    else:
+        await m.reply_text("you must specify the rules, E.g.: <code>/setrules (here the rules)</code>.")
         
 @Client.on_message(filters.command("restrules", prefix) & filters.group)
 @require_admin(permissions=["can_restrict_members"])
 async def delete_rules(c: Client, m: Message):
     set_rules(m.chat.id, None)
-    await m.reply_text("the rules was deleted successfully.")
+    await m.reply_text("the rules for this chat was deleted successfully.")
  
         
 @Client.on_message(filters.command("rules", prefix) & filters.group)
 async def show_rules(c: Client, m: Message):
-    rules = get_rules(m.chat.id) or "the rules was not setted"
+    rules = get_rules(m.chat.id) or "no rules has been set to this chat"
     await m.reply_text(f"the rules in the chat {m.chat.title}: \n\n {rules}")
