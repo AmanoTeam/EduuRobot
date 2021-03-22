0a0c04522a805fdfb47af6ecf1e700937584751a
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-03-22 21:21:35 +0200

    add rules and show error message if the target user of the warn is admin (#157)
    * add rules
    
    * Update rules.py
    
    * show error if the target user is admin
    
    * fix codacy error
    
    * fix codacy error
    
    * Lints and translations
    
    Co-authored-by: Alisson Lauffer <alissonvitortc@gmail.com>

---
+++ plugins/rules.py
@@ -0,0 +1,49 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from dbh import dbc, db
+from utils import require_admin
+from localization import use_chat_lang
+from config import prefix
+
+
+def get_rules(chat_id):
+    dbc.execute('SELECT rules FROM groups WHERE chat_id = (?)', (chat_id,))
+    try:
+        return dbc.fetchone()[0]
+    except IndexError:
+        return None
+
+
+def set_rules(chat_id, rules):
+    dbc.execute('UPDATE groups SET rules = ? WHERE chat_id = ?', (rules, chat_id))
+    db.commit()
+
+
+@Client.on_message(filters.command("setrules", prefix) & filters.group)
+@require_admin(permissions=["can_change_info"])
+@use_chat_lang()
+async def settherules(c: Client, m: Message, strings):
+    if len(m.text.split()) > 1:
+        set_rules(m.chat.id, m.text.split(None, 1)[1])
+        await m.reply_text(strings("rules_set_success").format(chat_title=m.chat.title))
+    else:
+        await m.reply_text(strings("rules_set_empty"))
+
+
+@Client.on_message(filters.command("resetrules", prefix) & filters.group)
+@require_admin(permissions=["can_change_info"])
+@use_chat_lang()
+async def delete_rules(c: Client, m: Message, strings):
+    set_rules(m.chat.id, None)
+    await m.reply_text(strings("rules_deleted"))
+
+
+@Client.on_message(filters.command("rules", prefix) & filters.group)
+@use_chat_lang()
+async def show_rules(c: Client, m: Message, strings):
+    rules = get_rules(m.chat.id)
+    if rules:
+        # TODO: Send rules in private plus a toggle for that.
+        await m.reply_text(strings("rules_message").format(chat_title=m.chat.title, rules=rules))
+    else:
+        await m.reply_text(strings("rules_empty"))
