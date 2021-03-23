7cae4a8ca0dd87f01e074d96cf72d2dff7081a5a
Alisson Lauffer <alissonvitortc@gmail.com>
2021-03-23 17:24:36 -0300

    Translations

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from config import prefix
 from dbh import dbc, db
+from localization import use_chat_lang
 from utils import require_admin
 
 
@@ -38,45 +39,51 @@ def disable_welcome(chat_id):
 
 
 @Client.on_message(filters.command("setwelcome", prefix) & filters.group)
-@require_admin()
-async def set_welcome_message(c: Client, m: Message):
+@require_admin(permissions=["can_change_info"])
+@use_chat_lang()
+async def set_welcome_message(c: Client, m: Message, strings):
     if len(m.text.split()) > 1:
         set_welcome(m.chat.id, m.text.split(None, 1)[1])
-        await m.reply_text(f"The welcome message for {m.chat.title} were set successfully")
+        await m.reply_text(strings("welcome_set_success").format(chat_title=m.chat.title))
     else:
-        await m.reply_text("You must specify the welcome message, E.g.: <code>/setwelcome (here the welcome message)</code>.")
+        await m.reply_text(strings("welcome_set_empty"))
 
 
 @Client.on_message(((filters.command("welcome") & ~filters.command(["welcome on", "welcome off"]))) & filters.group)
-@require_admin()
-async def invlaid_welcome_status_arg(c: Client, m: Message):
+@require_admin(permissions=["can_change_info"])
+@use_chat_lang()
+async def invlaid_welcome_status_arg(c: Client, m: Message, strings):
     enable_welcome(m.chat.id)
-    await m.reply_text("invalid argument use <code>/welcome off/on</code>.")
+    await m.reply_text(strings("welcome_mode_invalid"))
 
 
 @Client.on_message(filters.command("welcome on", prefix) & filters.group)
-@require_admin()
-async def enable_welcome_message(c: Client, m: Message):
+@require_admin(permissions=["can_change_info"])
+@use_chat_lang()
+async def enable_welcome_message(c: Client, m: Message, strings):
     enable_welcome(m.chat.id)
-    await m.reply_text(f"the welcome message for {m.chat.title} was enabled")
+    await m.reply_text(strings("welcome_mode_enable").format(chat_title=m.chat.title))
 
 
 @Client.on_message(filters.command("welcome off", prefix) & filters.group)
-@require_admin()
-async def disable_welcome_message(c: Client, m: Message):
+@require_admin(permissions=["can_change_info"])
+@use_chat_lang()
+async def disable_welcome_message(c: Client, m: Message, strings):
     disable_welcome(m.chat.id)
-    await m.reply_text(f"the welcome message for {m.chat.title} was disabled")
+    await m.reply_text(strings("welcome_mode_disable").format(chat_title=m.chat.title))
 
 
 @Client.on_message(filters.command(["resetwelcome", "clearwelcome"], prefix) & filters.group)
-@require_admin()
-async def reset_welcome_message(c: Client, m: Message):
+@require_admin(permissions=["can_change_info"])
+@use_chat_lang()
+async def reset_welcome_message(c: Client, m: Message, strings):
     set_welcome(m.chat.id, None)
-    await m.reply_text(f"The welcome message for {m.chat.title} were setted again to the default welcome message of the bot.")
+    await m.reply_text(strings("welcome_reset").format(chat_title=m.chat.title))
 
 
 @Client.on_message(filters.new_chat_members & filters.group)
-async def greet_new_members(c: Client, m: Message):
+@use_chat_lang()
+async def greet_new_members(c: Client, m: Message, strings):
     chat_title = m.chat.title
     chat_id = m.chat.id
     first_name = m.from_user.first_name
@@ -96,6 +103,6 @@ async def greet_new_members(c: Client, m: Message):
                 welcome = welcome.replace('$first_name', escape_markdown(first_name))
                 welcome = welcome.replace('$last_name', escape_markdown(last_name))
             else:
-                welcome = 'hey {}, welcome to {}!'.format(escape_markdown(first_name),
-                                                          escape_markdown(chat_title))
+                welcome = strings("welcome_default").format(user_name=escape_markdown(first_name),
+                                                            chat_title=escape_markdown(chat_title))
             await m.reply_text(welcome, parse_mode="markdown", disable_web_page_preview=True)
