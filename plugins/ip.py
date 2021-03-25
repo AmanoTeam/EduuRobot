a1d8096f0822e62f15aaa67e16d976b629159eee
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-03-25 20:51:30 +0200

    add inline to ip command (#166)

--- plugins/ip.py
+++ plugins/ip.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineQuery, InlineQueryResultArticle, InputTextMessageContent
 from config import prefix
 from consts import http
 import re
@@ -20,3 +20,22 @@ async def ip_cmd(c: Client, m: Message):
         await m.reply_text(x, parse_mode="html")
     else:
         await m.reply_text("You must specify the url, E.g.: <code>/ip example.com</code>")
+
+
+@Client.on_inline_query(filters.regex(r"^ip"))
+async def ip_inline(c: Client, q: InlineQuery):
+    getme = await c.get_me()
+    if len(q.query.split()) > 1:
+        text = q.query.split(maxsplit=1)[1]
+        if text.startswith("http"):
+            url = re.sub("http(s|)://", "", text)
+        else:
+            url = text
+        r = await http.get('http://ip-api.com/json/' + url)
+        req = r.json()
+        x = ""
+        for i in req:
+            x += "<b>{}</b>: <code>{}</code>\n".format(i.title(), req[i])
+        await q.answer([InlineQueryResultArticle(title=f"click here to see the ip of {text}", input_message_content=InputTextMessageContent(x, parse_mode="html"))])
+    else:
+        await q.answer([InlineQueryResultArticle(title="You must specify the url", input_message_content=InputTextMessageContent(f"You must specify the url, E.g.: <code>@{getme.username} ip example.com</code>", parse_mode="html"))])
