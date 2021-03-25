6e54764beaf83c2d06eb63c76a23d8808f7f3698
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-03-25 10:41:27 +0200

    add /ip command

---
+++ plugins/ip.py
@@ -0,0 +1,22 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import prefix
+from consts import http
+import re
+
+@Client.on_message(filters.command("ip", prefix))
+async def ip_cmd(c: Client, m: Message):
+    if len(m.text.split()) > 1:
+        text = m.text.split(maxsplit=1)[1]
+        if text.startswith("http"):
+            url = re.sub("http(s|)://", "", text)
+        else:
+            url = text
+        r = await http.get('http://ip-api.com/json/' + url)
+        req = r.json()
+        x = ""
+        for i in req:
+            x += "*{}*: `{}`\n".format(i.title(), req[i])
+        await m.reply_text(x, parse_mode="md")
+    else:
+        await m.reply_text("You must specify the url, E.g.: <code>/ip example.com</code>")
