18c8275c159ed110fdde2ccdb481401610d86826
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-03-27 11:48:35 +0300

    Update inlines.py

--- plugins/inlines.py
+++ plugins/inlines.py
@@ -110,7 +110,7 @@ async def info_inline(c: Client, q: InlineQuery):
             InlineQueryResultArticle(
                 title="click here to get the information about the user",
                 input_message_content=InputTextMessageContent(
-                    f"username: {user.username} \n id: {user.id} dc: {user.dc_id} \n link to the user: {user.mention(mentiontext)}",
+                    f"username: {user.username} \n id: {user.id} \n dc: {user.dc_id} \n link to the user: {user.mention(mentiontext)}",
                 ),
             )
         ]
