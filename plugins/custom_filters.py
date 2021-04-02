150f9cdea3c194f74c1f2e184802ec8f411685dd
ᴀɴᴀɴᴅ./ <62738084+Anandpskerala@users.noreply.github.com>
2021-04-02 20:50:15 +0530

    Filters somewhat fixed  (#180)
    * Reduced to text filters only due to some issues
    
    * Reduced to text filters only due to some issues
    
    * Update custom_filters.py
    
    Co-authored-by: iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>

--- plugins/custom_filters.py
+++ plugins/custom_filters.py
@@ -1,17 +1,47 @@
+import re
+
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup
 from config import prefix
 from localization import use_chat_lang
 from utils import require_admin, split_quotes, button_parser
 from dbh import dbc, db
 
 
-def add_filter(chat_id, trigger, text, button, file_id):
+def add_filter(chat_id, trigger, raw_data, file_id):
     dbc.execute(
-        "INSERT INTO filters(chat_id, filter_name, text, button, file) VALUES(?, ?, ?, ?, ?)",
-        (chat_id, trigger, text, button, file_id)
+        "INSERT INTO filters(chat_id, filter_name, raw_data, file_id) VALUES(?, ?, ?, ?)",
+        (chat_id, trigger, raw_data, file_id)
     )
     db.commit()
+
+
+def update_filter(chat_id, trigger, raw_data, file_id):
+    dbc.execute(
+        "UPDATE filters SET raw_data = ?, file_id = ? WHERE chat_id = ? AND filter_name = ?",
+        (chat_id, trigger, raw_data, file_id)
+    )
+    db.commit()
+
+
+def get_all_filters(chat_id):
+    dbc.execute(
+        "SELECT * FROM filters WHERE chat_id = ?",
+        (chat_id,)
+    )
+
+    db.commit()
+    return dbc.fetchall()
+
+
+def check_for_filters(chat_id, trigger):
+    all_filters = get_all_filters(chat_id)
+    for keywords in all_filters:
+        keyword = keywords[1]
+        if trigger == keyword:
+            return True
+        else:
+            return False
 
 
 @Client.on_message(filters.command("filter", prefixes=prefix))
@@ -27,33 +57,41 @@ async def save_filter(c: Client, m: Message):
             quote=True
         )
         return
-    if m.reply_to_message:
-        message = m.reply_to_message.photo or \
-            m.reply_to_message.video or \
-            m.reply_to_message.video_note or \
-            m.reply_to_message.document or \
-            m.reply_to_message.animation or \
-            m.reply_to_message.sticker
 
-        if message and m.reply_to_message.caption is not None:
-            file_id = message.file_id
-            if m.reply_to_message.reply_markup:
-                button = m.reply_to_message.reply_markup.inline_keyboard
-                text = m.reply_to_message.caption.markdown
-            else:
-                text, button = button_parser(m.reply_to_message.caption.markdown)
-
-        else:
-            file_id = message.file_id
-            text, button = button_parser(split_text[1])
-    else:
-        file_id = None
-        text, button = button_parser(split_text[1])
+    file_id = None
+    raw_data = split_text[1]
 
     chat_id = m.chat.id
-    add_filter(chat_id, trigger, text, button, file_id)
+    check_filter = check_for_filters(chat_id, trigger)
+    if check_filter:
+        update_filter(chat_id, trigger, raw_data, file_id)
+    else:
+        add_filter(chat_id, trigger, raw_data, file_id)
     await m.reply_text(
         f"Added filter **{trigger}**",
         quote=True,
         parse_mode="md"
     )
+
+
+@Client.on_message(filters.group & filters.text, group=-1)
+async def serve_filter(c: Client, m: Message):
+    chat_id = m.chat.id
+    text = m.text
+
+    all_filters = get_all_filters(chat_id)
+    for filter_s in all_filters:
+        keyword = filter_s[1]
+        pattern = r"( |^|[^\w])" + re.escape(keyword) + r"( |$|[^\w])"
+        if re.search(pattern, text, flags=re.IGNORECASE):
+            data, button = button_parser(filter_s[2])
+            await m.reply_text(
+                data,
+                quote=True,
+                parse_mode="md",
+                reply_markup=InlineKeyboardMarkup(
+                    button
+                )
+            )
+
+
