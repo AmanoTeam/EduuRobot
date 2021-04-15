bb7896895a963ace66a2c6abc516a801f2d93bc2
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-04-15 11:32:06 +0300

    Update stickers.py

--- plugins/stickers.py
+++ plugins/stickers.py
@@ -2,11 +2,23 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from config import prefix
 from localization import use_chat_lang
+import os
 
 
 @Client.on_message(filters.command("stickerid", prefix) & filters.reply)
 @use_chat_lang()
 async def getstickerid(c: Client, m: Message, strings):
     if m.reply_to_message.sticker:
-        stickerid = m.reply_to_message.sticker.file_id
-        await m.reply_text(strings("sticker_id_string").format(stickerid=stickerid))
+        await m.reply_text(strings("get_sticker_id_string").format(stickerid=m.reply_to_message.sticker.file_id))
+
+
+@Client.on_message(filters.command("getsticker", prefix) & filters.reply)
+@use_chat_lang()
+async def getstickeraspng(c: Client, m: Message, strings):
+    if m.reply_to_message.sticker:
+        if m.reply_to_message.sticker.is_animated:
+            await m.reply_text(strings("animated_not_supported"))
+        if not m.reply_to_message.sticker.is_animated:
+            thesticker = await m.reply_to_message.download(file_name="sticker.png")
+            await m.reply_to_message.reply_photo(thesticker)
+            os.remove(thesticker)
