c454886336dc93d5e2aa3d5b674017b1411a8ecb
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-04-15 10:13:25 +0300

    Update git.py

--- plugins/git.py
+++ plugins/git.py
@@ -6,31 +6,28 @@ from pyrogram.types import Message
 from config import prefix
 from consts import http
 from utils import commands
+from localization import use_chat_lang
 
 
 @Client.on_message(filters.command("git", prefix))
-async def git(c: Client, m: Message):
+@use_chat_lang()
+async def git(c: Client, m: Message, strings):
     if len(m.command) == 1:
         return await m.reply_text(
-            "Specify a username", reply_to_message_id=m.message_id
+            strings("no_username_err"), reply_to_message_id=m.message_id
         )
     text = m.text.split(maxsplit=1)[1]
     req = await http.get(f"https://api.github.com/users/{text}")
     res = req.json()
 
     if not res.get("login"):
-        return await m.reply_text("User not found", reply_to_message_id=m.message_id)
+        return await m.reply_text(strings("not_found_user"), reply_to_message_id=m.message_id)
 
     avatar = res["avatar_url"]
 
     anticache = quote_plus((await http.head(avatar)).headers["Last-Modified"])
 
-    caption_text = """<b>Name</b> : <code>{name}</code>
-<b>Username</b> : <code>{username}</code>
-<b>Location</b> : <code>{location}</code>
-<b>Type</b> : <code>{type}</code>
-<b>Bio</b> : <code>{bio}</code>
-    """
+    caption_text = strings("info_git_user")
     await m.reply_photo(
         avatar + anticache,
         caption=caption_text.format(
