336cc124770ae34e86296754a999f94b0da2623b
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-04-24 09:12:18 +0300

    Update telegraph.py

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -3,10 +3,12 @@ from pyrogram.types import Message
 from telegraph import upload_file
 
 from config import prefix
+from localization import use_chat_lang
 
 
 @Client.on_message(filters.command("telegraph", prefix))
-async def telegraph(c: Client, m: Message):
+@use_chat_lang()
+async def telegraph(c: Client, m: Message, strings):
     if m.reply_to_message:
         if (
             m.reply_to_message.photo
@@ -18,4 +20,4 @@ async def telegraph(c: Client, m: Message):
             tele_link = "https://telegra.ph" + media_urls[0]
             await m.reply_text(tele_link)
     else:
-        await m.reply_text("Please Reply to Photo or Video")
+        await m.reply_text(strings("telegraph_err_no_reply"))
