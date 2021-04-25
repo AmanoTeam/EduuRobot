b8ec64296ac9b33d26fc80646ea336c953464d3c
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-04-25 11:18:17 +0300

    Update tiorunexec.py

--- plugins/tiorunexec.py
+++ plugins/tiorunexec.py
@@ -20,7 +20,7 @@ async def exec_tio_run_code(c: Client, m: Message):
         tioreq = TioRequest(lang=execlanguage, code=codetoexec)
         sendtioreq = tio.send(tioreq)
         await m.reply_text(
-            f"language:\n\n <code>{execlanguage}</code>\n\n code:\n\n <code>{codetoexec}</code> \n\nresults: \n\n <code>{sendtioreq.result}</code> \n\n errors: \n\n<code>{sendtioreq.error}</code>"
+            f"language:\n\n<code>{execlanguage}</code>\n\ncode:\n\n<code>{codetoexec}</code>\n\nresults:\n\n<code>{sendtioreq.result}</code>\n\n errors:\n\n<code>{sendtioreq.error}</code>"
         )
     else:
         await m.reply_text(f"error: the language {execlanguage} not found, the supported languages list: https://nekobin.com/tavijipafa")
