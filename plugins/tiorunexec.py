24ac55fb2ee5b1fb088a33d922fc85737d069281
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-04-25 11:35:07 +0300

    Update tiorunexec.py

--- plugins/tiorunexec.py
+++ plugins/tiorunexec.py
@@ -1,6 +1,11 @@
 from pytio import Tio, TioRequest
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import (
+    Message,
+    InlineQuery,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+)
 from config import prefix
 
 tio = Tio()
@@ -23,4 +28,35 @@ async def exec_tio_run_code(c: Client, m: Message):
             f"language:\n\n<code>{execlanguage}</code>\n\ncode:\n\n<code>{codetoexec}</code>\n\nresults:\n\n<code>{sendtioreq.result}</code>\n\n errors:\n\n<code>{sendtioreq.error}</code>"
         )
     else:
-        await m.reply_text(f"error: the language {execlanguage} not found, the supported languages list: https://nekobin.com/tavijipafa")
+        await m.reply_text(
+            f"error: the language {execlanguage} not found, the supported languages list: https://nekobin.com/tavijipafa"
+        )
+
+
+@Client.on_inline_query(filters.regex(r"^exec"))
+async def tr_inline(c: Client, q: InlineQuery):
+    codetoexec = q.query.split(None, 2)[2]
+    execlanguage = q.query.lower().split()[1]
+    langslist = getlangs()
+    if execlanguage in langslist:
+        await q.answer(
+            [
+                InlineQueryResultArticle(
+                    title=f"language:\n\n<code>{execlanguage}</code>\n\ncode:\n\n<code>{codetoexec}</code>\n\nresults:\n\n<code>{sendtioreq.result}</code>\n\n errors:\n\n<code>{sendtioreq.error}</code>",
+                    input_message_content=InputTextMessageContent(
+                        f"language:\n\n<code>{execlanguage}</code>\n\ncode:\n\n<code>{codetoexec}</code>\n\nresults:\n\n<code>{sendtioreq.result}</code>\n\n errors:\n\n<code>{sendtioreq.error}</code>"
+                    ),
+                )
+            ]
+        )
+    else:
+        await q.answer(
+            [
+                InlineQueryResultArticle(
+                    title=f"error: the language {execlanguage} not found, the supported languages list: https://nekobin.com/tavijipafa",
+                    input_message_content=InputTextMessageContent(
+                        f"error: the language {execlanguage} not found, the supported languages list: https://nekobin.com/tavijipafa"
+                    ),
+                )
+            ]
+        )
