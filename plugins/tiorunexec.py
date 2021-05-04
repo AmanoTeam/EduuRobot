807e153ae99f225b450fc2e4591eca080b941ddb
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-05-04 14:23:02 +0300

    Update tiorunexec.py

--- plugins/tiorunexec.py
+++ plugins/tiorunexec.py
@@ -26,15 +26,24 @@ async def exec_tio_run_code(c: Client, m: Message, strings):
         loop = asyncio.get_event_loop()
         sendtioreq = await loop.run_in_executor(None, tio.send, tioreq)
         tioexeccodestats = sendtioreq.debug.decode()
-        await m.reply_text(
-            strings("code_exec_tio_res_string").format(
-                langformat=execlanguage,
-                codeformat=codetoexec,
-                resformat=sendtioreq.result,
-                errformat=sendtioreq.error,
-                statsformat=tioexeccodestats,
+        if error is None:
+            await m.reply_text(
+                strings("code_exec_tio_res_string_no_err").format(
+                    langformat=execlanguage,
+                    codeformat=codetoexec,
+                    resformat=sendtioreq.result,
+                    statsformat=tioexeccodestats,
+                )
             )
-        )
+        else:
+            await m.reply_text(
+                strings("code_exec_tio_res_string_err").format(
+                    langformat=execlanguage,
+                    codeformat=codetoexec,
+                    resformat=sendtioreq.result,
+                    errformat=sendtioreq.error,
+                )
+            )
     else:
         await m.reply_text(
             strings("code_exec_err_string").format(langformat=execlanguage)
