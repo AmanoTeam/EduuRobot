dfb5a64bf1cac603d41fb2d54f75c1ed47d34a9a
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-05-05 15:16:20 +0300

    remove the comment because this is done

--- plugins/rules.py
+++ plugins/rules.py
@@ -45,7 +45,6 @@ async def show_rules(c: Client, m: Message, strings):
     gettherules = get_rules(m.chat.id)
     rulestxt, rules_buttons = button_parser(gettherules)
     if rulestxt:
-        # TODO: Send rules in private plus a toggle for that.
         await m.reply_text(
             strings("rules_message").format(chat_title=m.chat.title, rules=rulestxt),
             reply_markup=(
