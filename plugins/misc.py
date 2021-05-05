b8ec43d2a9d1c525fb103e61c921f4d844959df4
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-05-05 18:53:44 +0300

    fix

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,5 +1,5 @@
 import re
-from html import escape
+from html import escape, unescape
 from urllib.parse import quote, unquote
 
 from pyrogram import Client, filters
@@ -13,7 +13,7 @@ from utils import commands, button_parser
 
 
 admin_status = ["creator", "administrator"]
-char = html.unescape('&#8204;')
+char = unescape('&#8204;')
 
 
 @Client.on_message(filters.command("mark", prefix))
