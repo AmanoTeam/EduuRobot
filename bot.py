d17aff0199cbaac75884ea7bbdb99b298b0c24c4
Alisson Lauffer <alissonvitortc@gmail.com>
2021-05-07 01:23:52 -0300

    treewide: Wrap main codes inside async def, drop pytest and pass api keys as keyword arguments

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
+import asyncio
 import logging
+import sys
 
-from config import API_HASH, API_ID, TOKEN, disabled_plugins, log_chat
 from pyrogram import Client, idle
 from pyrogram.errors import BadRequest
 
+from config import API_HASH, API_ID, TOKEN, disabled_plugins, log_chat
+from consts import http
 from utils import del_restarted, get_restarted
 
 with open("version.txt") as f:
@@ -12,26 +15,37 @@ with open("version.txt") as f:
 
 client = Client(
     "bot",
-    API_ID,
-    API_HASH,
+    api_id=API_ID,
+    api_hash=API_HASH,
     bot_token=TOKEN,
     workers=24,
     parse_mode="html",
     plugins=dict(root="plugins", exclude=disabled_plugins),
 )
 
-with client:
-    if __name__ == "__main__":
+
+async def main():
+    await client.start()
+
+    if "test" not in sys.argv:
         wr = get_restarted()
         del_restarted()
         try:
-            client.send_message(
+            await client.send_message(
                 log_chat,
-                ("<b>Bot started</b>\n\n" f"<b>Version:</b> <code>{version}</code>"),
+                f"<b>Bot started</b>\n\n<b>Version:</b> <code>{version}</code>",
             )
             if wr:
-                client.edit_message_text(wr[0], wr[1], "Restarted successfully!")
+                await client.edit_message_text(wr[0], wr[1], "Restarted successfully!")
         except BadRequest:
             logging.warning("Unable to send message to log_chat.")
             pass
-        idle()
+        await idle()
+
+    await http.aclose()
+    await client.stop()
+
+
+loop = asyncio.get_event_loop()
+
+loop.run_until_complete(main())
