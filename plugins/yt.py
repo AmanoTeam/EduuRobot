42948e6cb1af49301e8faa6e79a8e4364beaf827
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-05-13 09:45:40 +0300

    Update yt.py

--- plugins/yt.py
+++ plugins/yt.py
@@ -1,105 +1,178 @@
-import os, re, youtube_dl
-
+import datetime, os, re, tempfile, time, async_files, youtube_dl
+from pyromod.helpers import ikb
 from consts import http
-
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from config import prefix
+from pyrogram.errors import BadRequest, ImageProcessFailed
 from localization import use_chat_lang
+
 from utils import pretty_size
 
 
-ydl = youtube_dl.YoutubeDL(
-    {"outtmpl": "dls/%(title)s.%(ext)s", "format": "140", "noplaylist": True}
-)
+def aiowrap(func: Callable) -> Coroutine:
+    @wraps(func)
+    async def run(*args, loop=None, executor=None, **kwargs):
+        if loop is None:
+            loop = asyncio.get_event_loop()
+        pfunc = partial(func, *args, **kwargs)
+        return await loop.run_in_executor(executor, pfunc)
 
-yt_headers = {"x-youtube-client-name": "1", "x-youtube-client-version": "2.20200827"}
-
-
-async def search_yt(query):
-    url_base = "https://www.youtube.com/results"
-    url_yt = "https://www.youtube.com/watch?v="
-    r = await http.get(
-        url_base, params=dict(search_query=query, pbj="1"), headers=yt_headers
-    )
-    page = await r.json()
-    list_videos = []
-    for video in page[1]["response"]["contents"]["twoColumnSearchResultsRenderer"][
-        "primaryContents"
-    ]["sectionListRenderer"]["contents"][0]["itemSectionRenderer"]["contents"]:
-        if video.get("videoRenderer"):
-            vid_id = video["videoRenderer"]["videoId"]
-            title = video["videoRenderer"]["title"]["runs"][0]["text"]
-            dic = {"title": title, "url": url_yt + vid_id}
-            list_videos.append(dic)
-    return list_videos
+    return run
 
 
-@Client.on_message(filters.command("yt", prefix))
-@use_chat_lang()
-async def ytsearchcmd(c: Client, m: Message, strings):
-    res = await search_yt(m.text.split(None, 1)[1])
-    vids = [
-        "{}: <a href='{}'>{}</a>".format(num + 1, i["url"], i["title"])
-        for num, i in enumerate(res)
-    ]
-    await m.reply_text(
-        "\n".join(vids) if vids else strings("noresfound"),
-        disable_web_page_preview=True,
-        parse_mode="html",
-    )
+@aiowrap
+def extract_info(instance, url, download=True):
+    return instance.extract_info(url, download)
 
 
 @Client.on_message(filters.command("ytdl", prefix))
 @use_chat_lang()
 async def ytdlcmd(c: Client, m: Message, strings):
-    cmdargs = m.text.split(None, 1)[1]
-    if cmdargs:
-        sentmsg = await m.reply_text(strings("ytdlgetvidinfo"))
-        try:
-            yt = (
-                ydl.extract_info(cmdargs, download=False)
-                if re.match(
-                    r"^(https?://)?(youtu\.be/|(m\.|www\.)?youtube\.com/watch\?v=).+",
-                    cmdargs,
-                )
-                else ydl.extract_info("ytsearch:" + cmdargs, download=False)["entries"][
-                    0
-                ]
-            )
-            for f in yt["formats"]:
-                if f["format_id"] == "140":
-                    fsize = f["filesize"] or 0
-            name = yt["title"]
-        except Exception as e:
-            return await sentmsg.edit(strings("ytdlerrmsg").format(errmsg=e))
-        if not fsize > 200000000:
-            if " - " in name:
-                performer, title = name.rsplit(" - ", 1)
-            else:
-                performer = yt.get("creator") or yt.get("uploader")
-                title = name
-            await sentmsg.edit(
-                strings("ytdldownloadmainmsg").format(
-                    dwnldname=name, thedownloadsize=pretty_size(fsize)
-                ),
-                parse_mode="html",
-            )
-            ydl.download(["https://www.youtube.com/watch?v=" + yt["id"]])
-            await sentmsg.edit(strings("ytdlsendaudiomsg"))
-            await m.reply_audio(
-                audio=open(ydl.prepare_filename(yt), "rb"),
-                duration=yt["duration"],
-                performer=performer,
-                title=title,
-            )
-            os.remove(ydl.prepare_filename(yt))
-            await sentmsg.delete()
-        else:
-            await sentmsg.edit(
-                strings("ytdlerrfiletoobig").format(
-                    downloadsizeformat=pretty_size(fsize)
-                )
-            )
+    args = m.text.split(None, 1)[1]
+    user = m.from_user.id
+    if m.reply_to_message and m.reply_to_message.text:
+        url = m.reply_to_message.text
+    elif m.text and args:
+        url = args
     else:
-        await m.reply_text(strings("ytdlusemsg"), parse_mode="markdown")
+        await m.reply_text(strings("plspectxt"))
+        return
+    ydl = youtube_dl.YoutubeDL(
+        {"outtmpl": "dls/%(title)s-%(id)s.%(ext)s", "format": "mp4", "noplaylist": True}
+    )
+    rege = re.match(
+        r"http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?",
+        url,
+        re.M,
+    )
+    if not rege:
+        yt = await extract_info(ydl, "ytsearch:" + url, download=False)
+        yt = yt["entries"][0]
+    else:
+        yt = await extract_info(ydl, rege.group(), download=False)
+    for f in yt["formats"]:
+        if f["format_id"] == "140":
+            afsize = f["filesize"] or 0
+        if f["ext"] == "mp4" and not f["filesize"] is None:
+            vfsize = f["filesize"] or 0
+            vformat = f["format_id"]
+    keyboard = [
+        [
+            (
+                strings("ytdlaudbtn"),
+                f'_aud.{yt["id"]}|{afsize}|{vformat}|{m.chat.id}|{user}|{m.message_id}',
+            ),
+            (
+                strings("ytdlvidbtn"),
+                f'_vid.{yt["id"]}|{vfsize}|{vformat}|{m.chat.id}|{user}|{m.message_id}',
+            ),
+        ]
+    ]
+    if " - " in yt["title"]:
+        performer, title = yt["title"].rsplit(" - ", 1)
+    else:
+        performer = yt.get("creator") or yt.get("uploader")
+        title = yt["title"]
+
+    text = f"🎧 <b>{performer}</b> - <i>{title}</i>\n"
+    text += f"💾 <code>{pretty_size(afsize)}</code> (audio) / <code>{pretty_size(int(vfsize))}</code> (vídeo)\n"
+    text += f"⏳ <code>{datetime.timedelta(seconds=yt.get('duration'))}</code>"
+
+    await m.reply_text(text, reply_markup=c.ikb(keyboard))
+
+
+@Client.on_callback_query(filters.regex("^(_(vid|aud))"))
+@use_chat_lang()
+async def cli_ytdl(c: Client, cq: CallbackQuery, strings):
+    data, fsize, vformat, cid, userid, mid = cq.data.split("|")
+    if not cq.from_user.id == int(userid):
+        return await cq.answer("This button is not for you!", cache_time=60)
+    if int(fsize) > 200000000:
+        return await cq.answer(
+            (strings("ytdlerrfiletoobigmsg")),
+            show_alert=True,
+            cache_time=60,
+        )
+    vid = re.sub(r"^\_(vid|aud)\.", "", data)
+    url = "https://www.youtube.com/watch?v=" + vid
+    await cq.message.edit(strings("ytdldownloadmainmsg"))
+    await cq.answer("wait", cache_time=0)
+    with tempfile.TemporaryDirectory() as tempdir:
+        path = os.path.join(tempdir, "ytdl")
+    if "vid" in data:
+        ydl = youtube_dl.YoutubeDL(
+            {
+                "outtmpl": f"{path}/%(title)s-%(id)s.%(ext)s",
+                "format": vformat,
+                "noplaylist": True,
+            }
+        )
+    else:
+        ydl = youtube_dl.YoutubeDL(
+            {
+                "outtmpl": f"{path}/%(title)s-%(id)s.%(ext)s",
+                "format": "140",
+                "noplaylist": True,
+            }
+        )
+    try:
+        yt = await extract_info(ydl, url, download=True)
+    except BaseException as e:
+        await cq.message.edit(strings("ytdlerrmsg").format(errmsg=e))
+        return
+    a = "Sending..."
+    await cq.message.edit(a)
+    filename = ydl.prepare_filename(yt)
+    ctime = time.time()
+    r = await http.get(yt["thumbnail"])
+    async with async_files.FileIO(f"{path}/{ctime}.png", "wb") as f:
+        await f.write(r.read())
+        await f.close()
+    if "vid" in data:
+        try:
+            await c.send_video(
+                int(cid),
+                filename,
+                width=1920,
+                height=1080,
+                caption=yt["title"],
+                duration=yt["duration"],
+                thumb=f"{path}/{ctime}.png",
+                reply_to_message_id=int(mid),
+            )
+        except BadRequest as e:
+            await c.send_message(
+                chat_id=int(cid),
+                text=(strings("ytdlerrcantsndvidmsg").format(errmsg=e)),
+                reply_to_message_id=int(mid),
+            )
+            pass
+        await c.delete_messages(chat_id=int(cid), message_ids=cq.message.message_id)
+    else:
+        if " - " in yt["title"]:
+            performer, title = yt["title"].rsplit(" - ", 1)
+        else:
+            performer = yt.get("creator") or yt.get("uploader")
+            title = yt["title"]
+        try:
+            await c.send_audio(
+                int(cid),
+                filename,
+                title=title,
+                performer=performer,
+                duration=yt["duration"],
+                thumb=f"{path}/{ctime}.png",
+                reply_to_message_id=int(mid),
+            )
+        except BadRequest as e:
+            await c.send_message(
+                chat_id=int(cid),
+                text=(strings("ytdlerrcantsndaudmsg").format(errmsg=e)),
+                reply_to_message_id=int(mid),
+            )
+            pass
+        await c.delete_messages(chat_id=int(cid), message_ids=cq.message.message_id)
+    try:
+        os.system(f"rm -rf {tempdir}")
+    except BaseException:
+        return
