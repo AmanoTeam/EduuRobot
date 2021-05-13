587f41a93c91eaf065a7e8fec9ef4217f347731c
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-05-13 09:54:03 +0300

    Update yt.py

--- plugins/yt.py
+++ plugins/yt.py
@@ -1,4 +1,4 @@
-import datetime, os, re, tempfile, time, async_files, youtube_dl, asyncio
+import datetime, os, re, tempfile, time, async_files, youtube_dl, asyncio, math
 from pyromod.helpers import ikb
 from consts import http
 from pyrogram import Client, filters
