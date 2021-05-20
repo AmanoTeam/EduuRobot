337b2eee842acc24b201b0e3f307b41ed14d612b
Hitalo <hitalo331@outlook.com>
2021-05-20 10:21:51 -0300

    treewide: using `__all__` instead of `from module import *`

--- eduu/utils/__init__.py
+++ eduu/utils/__init__.py
@@ -1,4 +1,46 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2018-2021 Amano Team
 
-from .utils import *
+from .utils import (
+    EMOJI_PATTERN,
+    add_chat,
+    aiowrap,
+    button_parser,
+    chat_exists,
+    check_perms,
+    commands,
+    deEmojify,
+    del_restarted,
+    get_emoji_regex,
+    get_restarted,
+    pretty_size,
+    remove_escapes,
+    require_admin,
+    set_restarted,
+    shell_exec,
+    split_quotes,
+    sudofilter,
+    time_extract,
+)
+
+__all__ = [
+    "commands",
+    "shell_exec",
+    "deEmojify",
+    "get_emoji_regex",
+    "button_parser",
+    "split_quotes",
+    "remove_escapes",
+    "time_extract",
+    "require_admin",
+    "check_perms",
+    "set_restarted",
+    "get_restarted",
+    "del_restarted",
+    "chat_exists",
+    "add_chat",
+    "aiowrap",
+    "pretty_size",
+    "EMOJI_PATTERN",
+    "sudofilter",
+]
