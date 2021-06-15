7c11c9f85eae491154c0106dba0b08f8933db32f
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-06-15 21:52:36 +0300

    youtube: add youtube search command (#280)
    Co-authored-by: Alisson Lauffer <alissonvitortc@gmail.com>

--- eduu/plugins/youtube.py
+++ eduu/plugins/youtube.py
@@ -27,6 +27,40 @@ def extract_info(instance, url, download=True):
     return instance.extract_info(url, download)
 
 
+async def search_yt(query):
+    page = (
+        await http.get(
+            "https://www.youtube.com/results",
+            params=dict(search_query=query, pbj="1"),
+            headers={
+                "x-youtube-client-name": "1",
+                "x-youtube-client-version": "2.20200827",
+            },
+        )
+    ).json()
+    list_videos = []
+    for video in page[1]["response"]["contents"]["twoColumnSearchResultsRenderer"][
+        "primaryContents"
+    ]["sectionListRenderer"]["contents"][0]["itemSectionRenderer"]["contents"]:
+        if video.get("videoRenderer"):
+            dic = {
+                "title": video["videoRenderer"]["title"]["runs"][0]["text"],
+                "url": "https://www.youtube.com/watch?v="
+                + video["videoRenderer"]["videoId"],
+            }
+            list_videos.append(dic)
+    return list_videos
+
+
+@Client.on_message(filters.command("yt", prefix))
+async def yt_search_cmd(c: Client, m: Message):
+    vids = [
+        '{}: <a href="{}">{}</a>'.format(num + 1, i["url"], i["title"])
+        for num, i in enumerate(await search_yt(m.text.split(None, 1)[1]))
+    ]
+    await m.reply_text("\n".join(vids) if vids else r"¯\_(ツ)_/¯")
+
+
 @Client.on_message(filters.command("ytdl", prefix))
 @use_chat_lang()
 async def ytdlcmd(c: Client, m: Message, strings):
