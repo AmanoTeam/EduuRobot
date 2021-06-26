d3bfa6b68e21e74a677ad54d84fbe6a908baf34b
Alisson Lauffer <alissonvitortc@gmail.com>
2021-06-26 04:57:54 -0300

    database: use executescript instead of multiple execute calls

--- eduu/database/common.py
+++ eduu/database/common.py
@@ -7,29 +7,27 @@ db = sqlite3.connect("eduu/database/eduu.db")
 dbc = db.cursor()
 
 
-dbc.execute(
-    """CREATE TABLE IF NOT EXISTS groups (chat_id INTEGER PRIMARY KEY,
-                                          welcome,
-                                          welcome_enabled INTEGER,
-                                          rules,
-                                          warns_limit INTEGER,
-                                          chat_lang,
-                                          cached_admins,
-                                          antichannelpin,
-                                          delservicemsgs,
-                                          warn_action)"""
-)
+dbc.executescript(
+    """
+CREATE TABLE IF NOT EXISTS groups (chat_id INTEGER PRIMARY KEY,
+                                   welcome,
+                                   welcome_enabled INTEGER,
+                                   rules,
+                                   warns_limit INTEGER,
+                                   chat_lang,
+                                   cached_admins,
+                                   antichannelpin,
+                                   delservicemsgs,
+                                   warn_action);
 
-dbc.execute(
-    """CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY,
-                                         chat_lang)"""
-)
+CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY,
+                                  chat_lang);
 
-dbc.execute("""CREATE TABLE IF NOT EXISTS channels (chat_id INTEGER PRIMARY KEY)""")
+CREATE TABLE IF NOT EXISTS channels (chat_id INTEGER PRIMARY KEY);
 
-dbc.execute(
-    """CREATE TABLE IF NOT EXISTS was_restarted_at (chat_id INTEGER,
-                                                    message_id INTEGER)"""
+CREATE TABLE IF NOT EXISTS was_restarted_at (chat_id INTEGER,
+                                             message_id INTEGER);
+"""
 )
 
 
