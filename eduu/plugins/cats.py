8d7417738d9199852c9a744164fb886c2f5450f3
Alisson Lauffer <alissonvitortc@gmail.com>
2021-11-03 01:16:11 -0300

    cats: send as animation if url ends with .gif

--- eduu/plugins/cats.py
+++ eduu/plugins/cats.py
@@ -16,7 +16,10 @@ async def cat(c: Client, m: Message, strings):
     r = await http.get("https://api.thecatapi.com/v1/images/search")
     rj = r.json()
 
-    await m.reply_photo(rj[0]["url"], caption=strings("meow"))
+    if rj[0]["url"].endswith(".gif"):
+        await m.reply_animation(rj[0]["url"], caption=strings("meow"))
+    else:
+        await m.reply_photo(rj[0]["url"], caption=strings("meow"))
 
 
 commands.add_command("cat", "general")
