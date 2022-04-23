30b705307024357732e758be6a32eb73b7ab7996
Alisson Lauffer <alissonvitortc@gmail.com>
2022-04-22 23:17:29 -0300

    localization: place most used languages at the top

--- eduu/utils/localization.py
+++ eduu/utils/localization.py
@@ -14,22 +14,23 @@ from eduu.database import db, dbc
 from eduu.utils.consts import group_types
 
 enabled_locales: List[str] = [
+    "en-GB",  # English (United Kingdom)
+    "en-US",  # English (United States)
+    "pt-BR",  # Portuguese (Brazil)
+    "es-ES",  # Spanish
+    "fr-FR",  # French
+    "de-DE",  # German
+    "it-IT",  # Italian
+    "nl-NL",  # Dutch
+
     "ar-SA",  # Arabic
     "ckb-IR",  # Sorani (Kurdish)
-    "de-DE",  # German
-    "en-GB",  # English
-    "en-US",  # English (United States)
-    "es-ES",  # Spanish
     "fi-FI",  # Finnish
-    "fr-FR",  # French
     "he-IL",  # Hebrew
     "id-ID",  # Indonesian
-    "it-IT",  # Italian
     "ja-JP",  # Japanese
-    "nl-NL",  # Dutch
     "no-NO",  # Norwegian
     "pl-PL",  # Polish
-    "pt-BR",  # Portuguese (Brazil)
     "pt-BRe",  # Portuguese (Brazil, extended version)
     "pt-BR2",  # Portuguese (Brazil, informal version)
     "ro-RO",  # Romanian
