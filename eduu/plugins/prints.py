6c1696a224502dc4b498a4ac7a8a4772e8e35eed
Alisson Lauffer <alissonvitortc@gmail.com>
2022-12-19 19:09:54 -0300

    prints: rework plugin

--- eduu/plugins/prints.py
+++ eduu/plugins/prints.py
@@ -2,9 +2,11 @@
 # Copyright (c) 2018-2022 Amano Team
 
 import uuid
+from json import JSONDecodeError
 
 from httpx import HTTPError
 from pyrogram import Client, filters
+from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 
 from ..config import PREFIXES
@@ -14,60 +16,71 @@ from ..utils.localization import use_chat_lang
 
 @Client.on_message(filters.command("print", PREFIXES))
 @use_chat_lang()
-async def prints(c: Client, message: Message, strings):
-    msg = message.text
-    the_url = msg.split(" ", 1)
-    wrong = False
+async def prints(c: Client, m: Message, strings):
+    # Get the target URl from the message using Telegram entities.
+    # If there is no URL, try to get an URL from the replied message.
+    # If there is no URL in the replied message, fail.
 
-    if len(the_url) == 1:
-        if message.reply_to_message:
-            the_url = message.reply_to_message.text
-            if len(the_url) == 1:
-                wrong = True
-            else:
-                the_url = the_url[1]
-        else:
-            wrong = True
+    for entity in m.entities:
+        if entity.type == MessageEntityType.URL:
+            target_url = m.text[entity.offset : entity.offset + entity.length]
+            break
+        if entity.type == MessageEntityType.TEXT_LINK:
+            target_url = entity.url
+            break
     else:
-        the_url = the_url[1]
-
-    if wrong:
-        await message.reply_text(strings("print_usage"))
-        return
-
-    try:
-        sent = await message.reply_text(strings("taking_screenshot"))
-        res_json = await cssworker_url(target_url=the_url)
-    except BaseException as e:
-        await message.reply(f"<b>Failed due to:</b> <code>{e}</code>")
-        return
-
-    if res_json:
-        # {"url":"image_url","response_time":"147ms"}
-        image_url = res_json["url"]
-        if image_url:
-            try:
-                await message.reply_photo(image_url)
-                await sent.delete()
-            except BaseException:
-                # if failed to send the message, it's not API's
-                # fault.
-                # most probably there are some other kind of problem,
-                # for example it failed to delete its message.
-                # or the bot doesn't have access to send media in the chat.
+        if m.reply_to_message:
+            for entity in m.reply_to_message.entities:
+                if entity.type == MessageEntityType.URL:
+                    target_url = m.reply_to_message.text[
+                        entity.offset : entity.offset + entity.length
+                    ]
+                    break
+                if entity.type == MessageEntityType.TEXT_LINK:
+                    target_url = entity.url
+                    break
+            else:
+                await m.reply_text(strings("print_usage"))
                 return
         else:
-            await message.reply(
-                "Couldn't get url value, most probably API is not accessible."
-            )
+            await m.reply_text(strings("print_usage"))
+            return
+
+    sent = await m.reply_text(strings("taking_screenshot"))
+
+    try:
+        response = await screenshot_page(target_url)
+    except BaseException as e:
+        await sent.edit_text(f"<b>API returned an error:</b> <code>{e}</code>")
+        return
+
+    if response:
+        try:
+            await m.reply_photo(response)
+            await sent.delete()
+        except BaseException:
+            # if failed to send the message, it's not API's
+            # fault.
+            # most probably there are some other kind of problem,
+            # for example it failed to delete its message.
+            # or the bot doesn't have access to send media in the chat.
+            return
     else:
-        await message.reply("Failed because API is not responding, try again later.")
+        await m.reply_text(
+            "Couldn't get url value, most probably API is not accessible."
+        )
 
 
-async def cssworker_url(target_url: str):
-    url = "https://htmlcsstoimage.com/demo_run"
-    my_headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0",
+async def screenshot_page(target_url: str) -> str:
+    """
+    This function is used to get a screenshot of a website using htmlcsstoimage.com API.
+
+    :param target_url: The URL of the website to get a screenshot of.
+    :return: The URL of the screenshot.
+    """
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0",
     }
 
     data = {
@@ -81,10 +94,14 @@ async def cssworker_url(target_url: str):
     }
 
     try:
-        resp = await http.post(url, headers=my_headers, json=data)
-        return resp.json()
-    except HTTPError:
-        return None
+        resp = await http.post(
+            "https://htmlcsstoimage.com/demo_run", headers=headers, json=data
+        )
+        return resp.json()["url"]
+    except (JSONDecodeError, KeyError) as e:
+        raise "Screenshot API returned an invalid response." from e
+    except (KeyError, HTTPError) as e:
+        raise "Screenshot API seems offline. Try again later." from e
 
 
 commands.add_command("print", "tools")
