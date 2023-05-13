1ba44e6f55b27775cf144b7cfa79988e7f740ff5
Alisson Lauffer <alissonvitortc@gmail.com>
2023-05-13 20:56:16 -0300

    reddit: add reddit plugin
    Co-Authored-By: Lucas Alberto <lucas.alberto98@gmail.com>

---
+++ eduu/plugins/reddit.py
@@ -0,0 +1,63 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2018-2023 Amano LLC
+
+from html import escape
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from config import PREFIXES
+from eduu.utils.localization import use_chat_lang
+
+from ..utils import commands, http
+
+CHARACTER_LIMIT = 25
+
+
+def limit_length(title: str):
+    if len(title) > CHARACTER_LIMIT:
+        return title[:CHARACTER_LIMIT].rstrip() + "..."
+    else:
+        return title
+
+
+@Client.on_message(filters.command(["reddit", "r"], PREFIXES))
+@use_chat_lang()
+async def reddit(c: Client, m: Message, strings):
+    if len(m.command) == 1:
+        return await m.reply_text(strings("reddit_usage"))
+
+    subreddit = m.command[1]
+
+    r = await http.get(f"https://www.reddit.com/r/{subreddit}/.json?limit=6")
+
+    if r.status_code in (404, 403):
+        return await m.reply_text(strings("not_found"))
+
+    if r.status_code >= 300:
+        return await m.reply_text(strings("error"))
+
+    data = r.json()
+
+    feed_items = []
+    for post in data["data"]["children"]:
+        post_url = post["data"]["url"]
+        post_title = escape(limit_length(post["data"]["title"]))
+        nsfw = "NSFW" if post["data"]["over_18"] else "SFW"
+        comments = strings("comments").format(post["data"]["num_comments"])
+
+        post_item = f" - <a href='{post_url}'>{post_title}</a> &lt;{nsfw}&gt; - <a href='{post_url}'>{comments}</a>"
+        feed_items.append(post_item)
+
+    if not feed_items:
+        return await m.reply_text(strings("no_results"))
+
+    feed = "\n".join(feed_items)
+
+    await m.reply_text(
+        f"<a href='https://www.reddit.com/r/{subreddit}'>r/{subreddit}</a>\n\n{feed}",
+        disable_web_page_preview=True,
+    )
+
+
+commands.add_command("reddit", "tools")
